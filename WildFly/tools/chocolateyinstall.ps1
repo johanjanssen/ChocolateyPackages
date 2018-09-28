8071d0d6267671862c4ca11b9273eaaf8e053f8f
Johan Janssen <johanjanssen2001@gmail.com>
2018-09-28 07:59:46 +0200

    Added WildFly package

---
+++ WildFly/tools/chocolateyinstall.ps1
@@ -0,0 +1,6 @@
+﻿$packageName = $env:ChocolateyPackageName
+$targetDir   = 'C:\Program Files\WildFly'
+$url         = 'http://download.jboss.org/wildfly/14.0.1.Final/wildfly-14.0.1.Final.zip'
+$checksum    = 'a6bafc5c8c6c148648d9229c2066f041f8dc656c'
+
+Install-ChocolateyZipPackage $packageName $url $targetDir

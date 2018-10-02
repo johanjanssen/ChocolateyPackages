0085e3d5b1fa9ae623e888e22e0013cef0a5646c
Johan Janssen <j.janssen@rhea.infosupport.net>
2018-10-02 15:03:21 +0200

    Added uninstall feature.

--- OpenJDK/tools/chocolateyinstall.ps1
+++ OpenJDK/tools/chocolateyinstall.ps1
@@ -5,4 +5,5 @@ $checksum64  = 'fde3b28ca31b86a889c37528f17411cd0b9651beb6fa76cac89a223417910f4b
 
 Install-ChocolateyZipPackage $packageName $url64 $targetDir
 Install-ChocolateyEnvironmentVariable 'JAVA_HOME' $targetDir\jdk-11 'Machine'
-Install-ChocolateyPath '%JAVA_HOME%\bin' -PathType 'Machine'
+# The full path instead of the %JAVA_HOME% is needed so it can be removed with the Chocolatey Uninstall
+Install-ChocolateyPath 'C:\Program Files\OpenJDK\jdk-11\bin' -PathType 'Machine'
---
+++ OpenJDK/tools/chocolateyuninstall.ps1
@@ -0,0 +1,27 @@
+﻿Uninstall-ChocolateyEnvironmentVariable 'JAVA_HOME' 'Machine'
+rm -r 'C:\Program Files\OpenJDK\jdk-11'
+
+$pathToUnInstall = 'C:\Program Files\OpenJDK\jdk-11\bin'
+$pathType = 'Machine'
+
+if ($env:PATH.ToLower().Contains($pathToUnInstall.ToLower()))
+{
+	$statementTerminator = ";"
+	Write-Host "PATH environment variable contains $pathToUnInstall. Removing..."
+	$actualPath = [System.Collections.ArrayList](Get-EnvironmentVariable -Name 'Path' -Scope $pathType).split($statementTerminator)
+
+	$actualPath.Remove($pathToUnInstall)	
+	$newPath =  $actualPath -Join $statementTerminator
+
+	if ($pathType -eq [System.EnvironmentVariableTarget]::Machine) {
+		if (Test-ProcessAdminRights) {
+			Set-EnvironmentVariable -Name 'Path' -Value $newPath -Scope $pathType
+		} else {
+			$psArgs = "UnInstall-ChocolateyPath -pathToUnInstall `'$originalPathToUnInstall`' -pathType `'$pathType`'"
+			Start-ChocolateyProcessAsAdmin "$psArgs"
+		}
+	} else {
+		Set-EnvironmentVariable -Name 'Path' -Value $newPath -Scope $pathType
+	}
+}
+

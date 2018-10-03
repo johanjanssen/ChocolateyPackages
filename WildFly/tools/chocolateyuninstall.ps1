b8882d5ee1e1fd06186a307e382ca69b95c63e2f
Johan Janssen <j.janssen@rhea.infosupport.net>
2018-10-03 14:13:34 +0200

    Only remove if directory is empty.

--- WildFly/tools/chocolateyuninstall.ps1
+++ WildFly/tools/chocolateyuninstall.ps1
@@ -1,2 +1,8 @@
-﻿rm -r 'C:\Program Files\WildFly\wildfly-14.0.1.Final'
-rmdir 'C:\Program Files\WildFly' 
+﻿$WildFlyDirectory = 'C:\Program Files\WildFly' 
+
+rm -r 'C:\Program Files\WildFly\wildfly-14.0.1.Final'
+
+If ((Get-ChildItem -Force $WildFlyDirectory) -eq $Null) {
+    rmdir $WildFlyDirectory 
+}
+

4d59b6bbfe5fe99688a6cbf8c64094ad27d8e099
Johan Janssen <j.janssen@rhea.infosupport.net>
2018-10-04 07:58:28 +0200

    Added AdoptOpenJDK 11 hotspot package.

---
+++ AdoptOpenJDK/tools/chocolateyinstall.ps1
@@ -0,0 +1,9 @@
+﻿$packageName = $env:ChocolateyPackageName
+$targetDir   = 'C:\Program Files\AdoptOpenJDK'
+$url64       = 'https://github.com/AdoptOpenJDK/openjdk11-binaries/releases/download/jdk-11%2B28/OpenJDK11-jdk_x64_windows_hotspot_11_28.zip'
+$checksum64  = '311466765ef02a0a3450571417a63f72f515f00c6cd66cee7fcf630c03374e47'
+
+Install-ChocolateyZipPackage $packageName $url64 $targetDir
+Install-ChocolateyEnvironmentVariable 'JAVA_HOME' $targetDir\jdk-11+28 'Machine'
+# The full path instead of the %JAVA_HOME% is needed so it can be removed with the Chocolatey Uninstall
+Install-ChocolateyPath 'C:\Program Files\AdoptOpenJDK\jdk-11+28\bin' -PathType 'Machine'

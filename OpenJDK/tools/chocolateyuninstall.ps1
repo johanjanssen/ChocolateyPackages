fc343b99e32334a00d2744d83c8e0c3b1904f970
Franklin Yu <franklinyu@hotmail.com>
2018-11-29 10:28:08 -0500

    Make uninstallation portable

--- OpenJDK/tools/chocolateyuninstall.ps1
+++ OpenJDK/tools/chocolateyuninstall.ps1
@@ -1,1 +1,6 @@
-﻿Uninstall-ChocolateyEnvironmentVariable 'JAVA_HOME' 'Machine'
+﻿if (Test-ProcessAdminRights) {
+    $pathType = 'Machine'
+} else {
+    $pathType = 'User'
+}
+Uninstall-ChocolateyEnvironmentVariable 'JAVA_HOME' $pathType

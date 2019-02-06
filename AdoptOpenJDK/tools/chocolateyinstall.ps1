d5c4474e677d3e14c38af2cb4ed9cb2207e05573
Johan Janssen <j.janssen@rhea.infosupport.net>
2019-02-06 22:48:32 +0100

    New versions for AdoptOpenJDK and separate package for Java 8

--- AdoptOpenJDK/tools/chocolateyinstall.ps1
+++ AdoptOpenJDK/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$packageName = $env:ChocolateyPackageName
 $targetDir   = 'C:\Program Files\AdoptOpenJDK'
 $url64       = 'https://github.com/AdoptOpenJDK/openjdk11-binaries/releases/download/jdk-11.0.2%2B9/OpenJDK11U-jdk_x64_windows_hotspot_11.0.2_9.zip'
-$checksum64  = '305e2346534cd71dedbacddd2956bf5001bc8c5a79503b0f23c93a1c52031361'
+$checksum64  = 'bde1648333abaf49c7175c9ee8ba9115a55fc160838ff5091f07d10c4bb50b3a'
 
 Install-ChocolateyZipPackage $packageName $url64 $targetDir
 Install-ChocolateyEnvironmentVariable 'JAVA_HOME' $targetDir\jdk-11.0.2+9 'Machine'
---
+++ AdoptOpenJDK8/tools/chocolateyinstall.ps1
@@ -0,0 +1,9 @@
+﻿$packageName = $env:ChocolateyPackageName
+$targetDir   = 'C:\Program Files\AdoptOpenJDK'
+$url64       = 'https://github.com/AdoptOpenJDK/openjdk8-binaries/releases/download/jdk8u202-b08/OpenJDK8U-jdk_x64_windows_hotspot_8u202b08.zip'
+$checksum64  = '2637dab3bc81274e19991eebc27684276b482dd71d0f84fedf703d4fba3576e5'
+
+Install-ChocolateyZipPackage $packageName $url64 $targetDir
+Install-ChocolateyEnvironmentVariable 'JAVA_HOME' $targetDir\jdk8u202-b08 'Machine'
+# The full path instead of the %JAVA_HOME% is needed so it can be removed with the Chocolatey Uninstall
+Install-ChocolateyPath 'C:\Program Files\AdoptOpenJDK\jdk8u202-b08\bin' -PathType 'Machine'

91192a6c0f3509c8b088c719486a3d32db0a69a4
Johan Janssen <j.janssen@rhea.infosupport.net>
2019-03-04 08:03:52 +0100

    Added AdoptOpenJDK 11 JRE and fixed the source URL's of some other packages.

---
+++ AdoptOpenJDK11JRE/tools/chocolateyinstall.ps1
@@ -0,0 +1,9 @@
+﻿$packageName = $env:ChocolateyPackageName
+$targetDir   = 'C:\Program Files\AdoptOpenJDK'
+$url64       = 'https://github.com/AdoptOpenJDK/openjdk11-binaries/releases/download/jdk-11.0.2%2B9/OpenJDK11U-jre_x64_windows_hotspot_11.0.2_9.zip'
+$checksum64  = '7ea566935b171903dbf11a5ea4d0b2301393b9cc1454dd0e2f35ce1c825d7ec3'
+
+Install-ChocolateyZipPackage $packageName $url64 $targetDir
+Install-ChocolateyEnvironmentVariable 'JAVA_HOME' $targetDir\jdk-11.0.2+9-jre 'Machine'
+# The full path instead of the %JAVA_HOME% is needed so it can be removed with the Chocolatey Uninstall
+Install-ChocolateyPath 'C:\Program Files\AdoptOpenJDK\jdk-11.0.2+9-jre\bin' -PathType 'Machine'

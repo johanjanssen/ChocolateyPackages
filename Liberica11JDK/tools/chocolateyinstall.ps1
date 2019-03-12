567bd9b37bc2a247dc42d099ffb0bd22b3ecb2ed
Johan Janssen <j.janssen@rhea.infosupport.net>
2019-03-12 13:51:52 +0100

    Fixed Liberica download url

--- Liberica11JDK/tools/chocolateyinstall.ps1
+++ Liberica11JDK/tools/chocolateyinstall.ps1
@@ -1,6 +1,6 @@
 ﻿$packageName = $env:ChocolateyPackageName
 $targetDir   = 'C:\Program Files\Liberica'
-$url64       = 'https://download.java.net/java/GA/jdk11/9/GPL/Liberica-11.0.2_windows-x64_bin.zip'
+$url64       = 'https://github.com/bell-sw/Liberica/releases/download/11.0.2/bellsoft-jdk11.0.2-windows-amd64.zip'
 $checksum64  = '4F2109361256B33A15CD8A6E2C21BC473875C05A'
 
 Install-ChocolateyZipPackage $packageName $url64 $targetDir

04ec3f5d1428a71eedcfd036de0c8389ec99a6f9
Johan Janssen <j.janssen@rhea.infosupport.net>
2019-03-27 14:58:16 +0100

    Fixed install path for OpenJDK 12.

--- OpenJDK/tools/chocolateyinstall.ps1
+++ OpenJDK/tools/chocolateyinstall.ps1
@@ -4,6 +4,6 @@ $url64       = 'https://download.java.net/java/GA/jdk12/GPL/openjdk-12_windows-x
 $checksum64  = '35a8d018f420fb05fe7c2aa9933122896ca50bd23dbd373e90d8e2f3897c4e92'
 
 Install-ChocolateyZipPackage $packageName $url64 $targetDir
-Install-ChocolateyEnvironmentVariable 'JAVA_HOME' $targetDir\jdk-12.0.0 'Machine'
+Install-ChocolateyEnvironmentVariable 'JAVA_HOME' $targetDir\jdk-12 'Machine'
 # The full path instead of the %JAVA_HOME% is needed so it can be removed with the Chocolatey Uninstall
-Install-ChocolateyPath 'C:\Program Files\OpenJDK\jdk-12.0.0\bin' -PathType 'Machine'
+Install-ChocolateyPath 'C:\Program Files\OpenJDK\jdk-12\bin' -PathType 'Machine'
--- OpenJDK/tools/chocolateyuninstall.ps1
+++ OpenJDK/tools/chocolateyuninstall.ps1
@@ -1,7 +1,7 @@
 ﻿Uninstall-ChocolateyEnvironmentVariable 'JAVA_HOME' 'Machine'
-rm -r 'C:\Program Files\OpenJDK\jdk-12.0.0'
+rm -r 'C:\Program Files\OpenJDK\jdk-12'
 
-$pathToUnInstall = 'C:\Program Files\OpenJDK\jdk-12.0.0\bin'
+$pathToUnInstall = 'C:\Program Files\OpenJDK\jdk-12\bin'
 $pathType = 'Machine'
 
 if ($env:PATH.ToLower().Contains($pathToUnInstall.ToLower()))

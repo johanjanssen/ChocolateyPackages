978d7caae8ae54968244a79fc1c792706d3a1873
Johan Janssen <j.janssen@rhea.infosupport.net>
2019-04-12 10:02:29 +0200

    Changed download url.

--- SapMachine/tools/chocolateyinstall.ps1
+++ SapMachine/tools/chocolateyinstall.ps1
@@ -1,6 +1,6 @@
 ﻿$packageName = $env:ChocolateyPackageName
 $targetDir   = 'C:\Program Files\SapMachine'
-$url64       = 'https://github.com/SAP/SapMachine/releases/download/sapmachine-12/sapmachine-sapmachine-jdk-12-ea.33_windows-x64_bin.zip'
+$url64       = 'https://github.com/SAP/SapMachine/releases/download/sapmachine-12/sapmachine-jdk-12-ea.33_windows-x64_bin.zip'
 $checksum64  = 'a792c41dbe807d7e404b0562d69dc2f139ed37ea74f23f2d0f69bc6bff39df3a'
 
 Install-ChocolateyZipPackage $packageName $url64 $targetDir

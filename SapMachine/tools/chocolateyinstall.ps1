72b13e3ba8d9507d86826116e00872eb199b69a5
Johan Janssen <j.janssen@rhea.infosupport.net>
2019-04-19 07:46:12 +0200

    Upgrade to latest version of 12.

--- SapMachine/tools/chocolateyinstall.ps1
+++ SapMachine/tools/chocolateyinstall.ps1
@@ -1,9 +1,9 @@
 ﻿$packageName = $env:ChocolateyPackageName
 $targetDir   = 'C:\Program Files\SapMachine'
-$url64       = 'https://github.com/SAP/SapMachine/releases/download/sapmachine-12/sapmachine-jdk-12-ea.33_windows-x64_bin.zip'
-$checksum64  = 'a792c41dbe807d7e404b0562d69dc2f139ed37ea74f23f2d0f69bc6bff39df3a'
+$url64       = 'https://github.com/SAP/SapMachine/releases/download/sapmachine-12.0.1/sapmachine-jdk-12.0.1_windows-x64_bin.zip'
+$checksum64  = '94be7be2a9c201f8ea4718be168b461b6c01043b48801ff67cf32c801a32b5e5'
 
 Install-ChocolateyZipPackage $packageName $url64 $targetDir
-Install-ChocolateyEnvironmentVariable 'JAVA_HOME' $targetDir\sapmachine-jdk-12 'Machine'
+Install-ChocolateyEnvironmentVariable 'JAVA_HOME' $targetDir\sapmachine-jdk-12.0.1 'Machine'
 # The full path instead of the %JAVA_HOME% is needed so it can be removed with the Chocolatey Uninstall
-Install-ChocolateyPath 'C:\Program Files\SapMachine\sapmachine-jdk-12\bin' -PathType 'Machine'
+Install-ChocolateyPath 'C:\Program Files\SapMachine\sapmachine-jdk-12.0.1\bin' -PathType 'Machine'

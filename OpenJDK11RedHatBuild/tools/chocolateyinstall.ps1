9027390196bd32beb2d9b38d72b3370d6c6364aa
Johan Janssen <j.janssen@rhea.infosupport.net>
2019-04-19 11:02:40 +0200

    Added Red Hat builds of OpenJDK

---
+++ OpenJDK11RedHatBuild/tools/chocolateyinstall.ps1
@@ -0,0 +1,9 @@
+﻿$packageName = $env:ChocolateyPackageName
+$targetDir   = 'C:\Program Files\OpenJDK'
+$url64       = 'https://github.com/AdoptOpenJDK/openjdk11-upstream-binaries/releases/download/jdk-11.0.3%2B7/OpenJDK11U-x64_windows_11.0.3_7.zip'
+$checksum64  = '595BA894D2CF7857FD59D8532FFD6D25E80211F7012EBCB4E5A9258770412484'
+
+Install-ChocolateyZipPackage $packageName $url64 $targetDir
+Install-ChocolateyEnvironmentVariable 'JAVA_HOME' $targetDir\openjdk-11u-11.0.3+7 'Machine'
+# The full path instead of the %JAVA_HOME% is needed so it can be removed with the Chocolatey Uninstall
+Install-ChocolateyPath 'C:\Program Files\OpenJDK\openjdk-11u-11.0.3+7\bin' -PathType 'Machine'
---
+++ OpenJDK8RedHatBuild/tools/chocolateyinstall.ps1
@@ -0,0 +1,9 @@
+﻿$packageName = $env:ChocolateyPackageName
+$targetDir   = 'C:\Program Files\OpenJDK'
+$url64       = 'https://github.com/AdoptOpenJDK/openjdk8-upstream-binaries/releases/download/jdk8u212-b03/OpenJDK8u-x64_windows_8u212b03.zip'
+$checksum64  = '9942E99168D1D582820A2DB56922A702F3909379E1E302DE983D7DF9111AAD8F'
+
+Install-ChocolateyZipPackage $packageName $url64 $targetDir
+Install-ChocolateyEnvironmentVariable 'JAVA_HOME' $targetDir\openjdk-8u212-b03 'Machine'
+# The full path instead of the %JAVA_HOME% is needed so it can be removed with the Chocolatey Uninstall
+Install-ChocolateyPath 'C:\Program Files\OpenJDK\openjdk-8u212-b03\bin' -PathType 'Machine'

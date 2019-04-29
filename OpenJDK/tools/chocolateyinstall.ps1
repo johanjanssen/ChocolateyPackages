ae4fb28990807e8b433024b8654e498c093a3877
Johan Janssen <j.janssen@rhea.infosupport.net>
2019-04-29 17:13:08 +0200

    Upgraded OpenJDK to 12.0.1

--- OpenJDK/tools/chocolateyinstall.ps1
+++ OpenJDK/tools/chocolateyinstall.ps1
@@ -1,9 +1,9 @@
 ﻿$packageName = $env:ChocolateyPackageName
 $targetDir   = 'C:\Program Files\OpenJDK'
-$url64       = 'https://download.java.net/java/GA/jdk12/GPL/openjdk-12_windows-x64_bin.zip'
-$checksum64  = '35a8d018f420fb05fe7c2aa9933122896ca50bd23dbd373e90d8e2f3897c4e92'
+$url64       = 'https://download.oracle.com/java/GA/jdk12.0.1/69cfe15208a647278a19ef0990eea691/12/GPL/openjdk-12.0.1_windows-x64_bin.zip'
+$checksum64  = 'fc7d9eee3c09ea6548b00ca25dbf34a348b3942c815405a1428e0bfef268d08d'
 
 Install-ChocolateyZipPackage $packageName $url64 $targetDir
-Install-ChocolateyEnvironmentVariable 'JAVA_HOME' $targetDir\jdk-12 'Machine'
+Install-ChocolateyEnvironmentVariable 'JAVA_HOME' $targetDir\jdk-12.0.1 'Machine'
 # The full path instead of the %JAVA_HOME% is needed so it can be removed with the Chocolatey Uninstall
-Install-ChocolateyPath 'C:\Program Files\OpenJDK\jdk-12\bin' -PathType 'Machine'
+Install-ChocolateyPath 'C:\Program Files\OpenJDK\jdk-12.0.1\bin' -PathType 'Machine'
--- OpenJDK/tools/chocolateyuninstall.ps1
+++ OpenJDK/tools/chocolateyuninstall.ps1
@@ -1,7 +1,7 @@
 ﻿Uninstall-ChocolateyEnvironmentVariable 'JAVA_HOME' 'Machine'
-rm -r 'C:\Program Files\OpenJDK\jdk-12'
+rm -r 'C:\Program Files\OpenJDK\jdk-12.0.1'
 
-$pathToUnInstall = 'C:\Program Files\OpenJDK\jdk-12\bin'
+$pathToUnInstall = 'C:\Program Files\OpenJDK\jdk-12.0.1\bin'
 $pathType = 'Machine'
 
 if ($env:PATH.ToLower().Contains($pathToUnInstall.ToLower()))

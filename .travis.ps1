bc98b326e08f2f8bb7734817e9ddf3040c1304c6
cclauss <cclauss@me.com>
2019-05-13 21:27:10 +0200

    Can we add a shebang line to a .ps1 file?!?
    #!/c/Windows/System32/WindowsPowerShell/v1.0/powershell

--- .travis.ps1
+++ .travis.ps1
@@ -1,3 +1,5 @@
+#!/c/Windows/System32/WindowsPowerShell/v1.0/powershell
+
 choco install $Env:PACKAGE
 # refreshenv  # <-- useless
 $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User") 

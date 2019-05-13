9c5f51cec9ae580506d1db31cea784c0c1386471
Johan Janssen <j.janssen@rhea.infosupport.net>
2019-05-13 12:42:27 +0200

    test

--- AdoptOpenJDKJRE/tools/chocolateyinstall.ps1
+++ AdoptOpenJDKJRE/tools/chocolateyinstall.ps1
@@ -1,6 +1,6 @@
 ﻿$packageName = $env:ChocolateyPackageName
 $targetDir   = 'C:\Program Files\AdoptOpenJDK'
-$url64       = 'https://github.com/AdoptOpenJDK/openjdk12-binaries/releases/download/jdk-12%2B33/OpenJDK12U-jre_x64_windows_hotspot_12_33.zip'
+$url64       = 'https://git111111111111111111111111111hub.com/AdoptOpenJDK/openjdk12-binaries/releases/download/jdk-12%2B33/OpenJDK12U-jre_x64_windows_hotspot_12_33.zip'
 $checksum64  = '37dfdcf4d81387ad8c45161fcaba9e5a82778c55cf49d938b98b367b4ba2b99e'
 
 Install-ChocolateyZipPackage $packageName $url64 $targetDir

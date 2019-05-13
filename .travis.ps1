0552f617d147af7cee2041c93436adf918a2b519
cclauss <cclauss@me.com>
2019-05-13 21:30:51 +0200

    Use $PACKAGE inside the .ps1 file?

--- .travis.ps1
+++ .travis.ps1
@@ -1,7 +1,7 @@
 #!/c/Windows/System32/WindowsPowerShell/v1.0/powershell
 
-choco install $Env:PACKAGE
-# refreshenv  # <-- useless
-$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User") 
+choco install $PACKAGE
+refreshenv  # <-- useless
+# $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User") 
 echo $Env:Path
 java -version

e820e460c43411c33d5897dfe2f704e438880657
cclauss <cclauss@me.com>
2019-05-14 08:37:01 +0200

    $Env:Package --> $Env:PACKAGE

--- .travis.ps1
+++ .travis.ps1
@@ -1,4 +1,4 @@
-choco install $Env:Package
+choco install $Env:PACKAGE
 # refreshenv  # <-- useless
 $Env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User") 
 echo $Env:Path

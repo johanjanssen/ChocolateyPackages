f291d5c383c70ede22cd9df9829748a34de41e26
Johan Janssen <j.janssen@rhea.infosupport.net>
2019-05-16 12:41:30 +0200

    Revert "Changed download URL's so build should fail."
    This reverts commit fe3ecae28217332087e7cc5edf98f6ffafe043c6.

--- AdoptOpenJDK/tools/chocolateyinstall.ps1
+++ AdoptOpenJDK/tools/chocolateyinstall.ps1
@@ -1,6 +1,6 @@
 ﻿$packageName = $env:ChocolateyPackageName
 $targetDir   = 'C:\Program Files\AdoptOpenJDK'
-$url64       = 'https://11111github.com/AdoptOpenJDK/openjdk12-binaries/releases/download/jdk-12%2B33/OpenJDK12U-jdk_x64_windows_hotspot_12_33.zip'
+$url64       = 'https://github.com/AdoptOpenJDK/openjdk12-binaries/releases/download/jdk-12%2B33/OpenJDK12U-jdk_x64_windows_hotspot_12_33.zip'
 $checksum64  = 'b2ed490d2ecb6ec30894fcea5d19616ac91d6d3130133a2a5e904e241ce91f33'
 
 Install-ChocolateyZipPackage $packageName $url64 $targetDir

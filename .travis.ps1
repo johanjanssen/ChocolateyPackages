dda292e7f1560b3fa609202a75d998fbb3534ad6
Johan Janssen <j.janssen@rhea.infosupport.net>
2019-05-16 08:31:34 +0200

    exit

--- .travis.ps1
+++ .travis.ps1
@@ -1,12 +1,12 @@
 cd $Env:PACKAGE
 choco pack
-choco install $Env:PACKAGE -dv --force -s .
+choco install $Env:PACKAGE --force -s .
 $Env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User") 
 
 $error.clear()
 try { 
 	java -version
-} catch { travis_terminate 1; }
+} catch { exit }
 
 
 

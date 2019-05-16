efc2e40e7c13dbad1e00a3a579f3606c3846e326
Johan Janssen <j.janssen@rhea.infosupport.net>
2019-05-16 11:39:59 +0200

    Using version info from file.

--- .travis.ps1
+++ .travis.ps1
@@ -1,16 +1,17 @@
 cd $Env:PACKAGE
 choco pack
+
+
+
 choco install $Env:PACKAGE --force -s .
 $Env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User") 
 
 If ($Env:PACKAGE -eq "WildFly") {
 	$error.clear()	
 	try { 
-		Write-Host "try"
-		cd "C:\Program Files\WildFly\wildfly-16.0.0.Final\bin"
-		(Get-Item -Path ".\").FullName
-		echo "Press continue" |.\standalone.bat --version
-		Write-Host "end"
+		$basename = Get-ChildItem -Path $path -Recurse -Filter "wildfly.*.nupkg" | select -expandproperty basename
+		$version = $basename -replace 'wildfly.',''
+		Get-Content "C:\Program Files\WildFly\wildfly-$version.Final\modules\system\layers\base\org\jboss\as\product\wildfly-full\dir\META-INF\MANIFEST.MF"
 	} catch { exit 1 }
 	exit 0
 }

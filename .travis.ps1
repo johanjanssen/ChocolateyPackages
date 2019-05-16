68a2a71fc2b4bde1bf40a61b3a37eb0d498f528f
Johan Janssen <j.janssen@rhea.infosupport.net>
2019-05-16 12:17:32 +0200

    Test-Path

--- .travis.ps1
+++ .travis.ps1
@@ -1,16 +1,21 @@
 cd $Env:PACKAGE
-choco pack
-choco install $Env:PACKAGE --force -s .
+try {
+	choco pack
+	choco install $Env:PACKAGE --force -s .
+} catch { exit 1}
 $Env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User") 
 
 If ($Env:PACKAGE -eq "WildFly") {
-	$error.clear()	
-	try { 
-		$basename = Get-ChildItem -Path $path -Recurse -Filter "wildfly.*.nupkg" | select -expandproperty basename
-		$version = $basename -replace 'wildfly.',''
-		Get-Content "C:\Program Files\WildFly\wildfly-$version.Final\modules\system\layers\base\org\jboss\as\product\wildfly-full\dir\META-INF\MANIFEST.MF"
-	} catch { exit 1 }
-	exit 0
+	$basename = Get-ChildItem -Path $path -Recurse -Filter "wildfly.*.nupkg" | select -expandproperty basename
+	$version = $basename -replace 'wildfly.',''
+	$file = "C:\Program Files\WildFly\wildfly-$version.Final\modules\system\layers\base\org\jboss\as\product\wildfly-full\dir\META-INF\MANIFEST.MF"
+	If (Test-Path $file) {
+		Get-Content $file
+		exit 0
+	}
+	Else {
+		exit 1
+	}
 }
 
 $error.clear()

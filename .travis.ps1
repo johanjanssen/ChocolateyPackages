58aa5543206c4c663f5e9e23ad536c896aa2be37
Johan Janssen <j.janssen@rhea.infosupport.net>
2019-05-16 10:34:50 +0200

    Exception for WildFly

--- .travis.ps1
+++ .travis.ps1
@@ -3,6 +3,15 @@ choco pack
 choco install $Env:PACKAGE --force -s .
 $Env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User") 
 
+If ($Env:PACKAGE -eq WildFly) {
+	$error.clear()
+	try { 
+		cd C:\Program Files\WildFly\wildfly-16.0.0.Final\bin
+		echo/|standalone.bat --version
+	} catch { exit 1 }
+	exit 0
+}
+
 $error.clear()
 try { 
 	java -version

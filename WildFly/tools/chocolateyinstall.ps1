fe3ecae28217332087e7cc5edf98f6ffafe043c6
Johan Janssen <j.janssen@rhea.infosupport.net>
2019-05-16 11:59:52 +0200

    Changed download URL's so build should fail.

--- WildFly/tools/chocolateyinstall.ps1
+++ WildFly/tools/chocolateyinstall.ps1
@@ -1,6 +1,6 @@
 ﻿$packageName = $env:ChocolateyPackageName
 $targetDir   = 'C:\Program Files\WildFly'
-$url         = 'https://download.jboss.org/wildfly/16.0.0.Final/wildfly-16.0.0.Final.zip'
+$url         = 'https://1111111download.jboss.org/wildfly/16.0.0.Final/wildfly-16.0.0.Final.zip'
 $checksum    = 'b55ed374ab4f8534c2343ac9a283c71fa6d22dfe'
 
 Install-ChocolateyZipPackage $packageName $url $targetDir

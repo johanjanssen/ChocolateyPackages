f17301d44aa88974e7321c253a353705cad74bbb
Johan Janssen <j.janssen@rhea.infosupport.net>
2019-07-30 21:31:04 +0200

    New AdoptOpenJDK 12.0.2

--- AdoptOpenJDK/tools/chocolateyinstall.ps1
+++ AdoptOpenJDK/tools/chocolateyinstall.ps1
@@ -1,9 +1,9 @@
 ﻿$packageName = $env:ChocolateyPackageName
 $targetDir   = 'C:\Program Files\AdoptOpenJDK'
-$url64       = 'https://github.com/AdoptOpenJDK/openjdk12-binaries/releases/download/jdk-12.0.1%2B12/OpenJDK12U-jdk_x64_windows_hotspot_12.0.1_12.zip'
-$checksum64  = '2f8d6cfab85c62b607624778a2f63407dbefc8d3ea43e488cfb7c3b5a04a33c0'
+$url64       = 'https://github.com/AdoptOpenJDK/openjdk12-binaries/releases/download/jdk-12.0.2%2B10/OpenJDK12U-jdk_x64_windows_hotspot_12.0.2_10.zip'
+$checksum64  = 'e18f523cb71d7928cdb5fe8e567ab5f7336b3674f920e9e0d4052ba9639964a6'
 
 Install-ChocolateyZipPackage $packageName $url64 $targetDir
-Install-ChocolateyEnvironmentVariable 'JAVA_HOME' $targetDir\jdk-12.0.1+12 'Machine'
+Install-ChocolateyEnvironmentVariable 'JAVA_HOME' $targetDir\jdk-12.0.2+10 'Machine'
 # The full path instead of the %JAVA_HOME% is needed so it can be removed with the Chocolatey Uninstall
-Install-ChocolateyPath 'C:\Program Files\AdoptOpenJDK\jdk-12.0.1+12\bin' -PathType 'Machine'
+Install-ChocolateyPath 'C:\Program Files\AdoptOpenJDK\jdk-12.0.2+10\bin' -PathType 'Machine'

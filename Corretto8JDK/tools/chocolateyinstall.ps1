64167405d77dade879c509f31c6e4b7bcfe29df0
afischer211 <fischer.alexander@web.de>
2019-08-15 16:44:39 +0200

    upgraded Corretto to version 8.222.10.3

--- Corretto8JDK/tools/chocolateyinstall.ps1
+++ Corretto8JDK/tools/chocolateyinstall.ps1
@@ -1,9 +1,9 @@
 ﻿$packageName = $env:ChocolateyPackageName
 $targetDir   = 'C:\Program Files\Corretto'
-$url64       = 'https://d3pxv6yz143wms.cloudfront.net/8.212.04.1/amazon-corretto-8.212.04.1-windows-x64-jdk.zip'
-$checksum64  = 'e041513afec9ddea6976a62a19f31a0f'
+$url64       = 'https://d3pxv6yz143wms.cloudfront.net/8.222.10.1/amazon-corretto-8.222.10.3-windows-x64-jdk.zip'
+$checksum64  = '9879a7f69c0bd7d8c1bbb916df7b5f82'
 
 Install-ChocolateyZipPackage $packageName $url64 $targetDir
-Install-ChocolateyEnvironmentVariable 'JAVA_HOME' $targetDir\jdk1.8.0_212 'Machine'
+Install-ChocolateyEnvironmentVariable 'JAVA_HOME' $targetDir\jdk1.8.0_222 'Machine'
 # The full path instead of the %JAVA_HOME% is needed so it can be removed with the Chocolatey Uninstall
-Install-ChocolateyPath 'C:\Program Files\Corretto\jdk1.8.0_212\bin' -PathType 'Machine'
+Install-ChocolateyPath 'C:\Program Files\Corretto\jdk1.8.0_222\bin' -PathType 'Machine'

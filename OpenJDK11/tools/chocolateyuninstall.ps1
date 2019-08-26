0878ddd914133b8c33635c5c2ea1974a1008b03c
Johan Janssen <j.janssen@rhea.infosupport.net>
2019-08-26 13:30:14 +0200

    Upgrade OpenJDK to 11.0.4

--- OpenJDK11/tools/chocolateyuninstall.ps1
+++ OpenJDK11/tools/chocolateyuninstall.ps1
@@ -1,7 +1,7 @@
 ﻿Uninstall-ChocolateyEnvironmentVariable 'JAVA_HOME' 'Machine'
-rm -r 'C:\Program Files\OpenJDK\jdk-11.0.2'
+rm -r 'C:\Program Files\OpenJDK\openjdk-11u-11.0.4_11'
 
-$pathToUnInstall = 'C:\Program Files\OpenJDK\jdk-11.0.2\bin'
+$pathToUnInstall = 'C:\Program Files\OpenJDK\openjdk-11u-11.0.4_11\bin'
 $pathType = 'Machine'
 
 if ($env:PATH.ToLower().Contains($pathToUnInstall.ToLower()))

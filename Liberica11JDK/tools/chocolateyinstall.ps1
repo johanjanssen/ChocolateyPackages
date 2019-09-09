0fdf2e3f2c9ded2d86ca8a27bca29e3fd25dc5ad
Johan Janssen <j.janssen@rhea.infosupport.net>
2019-09-09 14:24:43 +0200

    Upgraded LibericaJDK to 11.0.4 and 12.0.2

--- Liberica11JDK/tools/chocolateyinstall.ps1
+++ Liberica11JDK/tools/chocolateyinstall.ps1
@@ -1,12 +1,12 @@
 ﻿$packageArgs = @{
     PackageName      = $env:ChocolateyPackageName
     UnzipLocation    = $targetDir = 'C:\Program Files\Liberica'
-    Url64            = 'https://github.com/bell-sw/Liberica/releases/download/11.0.2/bellsoft-jdk11.0.2-windows-amd64.zip'
-    Checksum64       = '4F2109361256B33A15CD8A6E2C21BC473875C05A'
+    Url64            = 'https://download.bell-sw.com/java/11.0.4/bellsoft-jdk11.0.4-windows-amd64.zip'
+    Checksum64       = '6F50B5E2C8EEDC24ECA0C598265D9B6310A9463A'
     ChecksumType64   = 'sha1'
 }
 
 Install-ChocolateyZipPackage @packageArgs
-Install-ChocolateyEnvironmentVariable 'JAVA_HOME' $targetDir\jdk-11.0.2 'Machine'
+Install-ChocolateyEnvironmentVariable 'JAVA_HOME' $targetDir\jdk-11.0.4 'Machine'
 # The full path instead of the %JAVA_HOME% is needed so it can be removed with the Chocolatey Uninstall
-Install-ChocolateyPath 'C:\Program Files\Liberica\jdk-11.0.2\bin' -PathType 'Machine'
+Install-ChocolateyPath 'C:\Program Files\Liberica\jdk-11.0.4\bin' -PathType 'Machine'
--- LibericaJDK/tools/chocolateyinstall.ps1
+++ LibericaJDK/tools/chocolateyinstall.ps1
@@ -1,12 +1,12 @@
 ﻿$packageArgs = @{
     PackageName      = $env:ChocolateyPackageName
     UnzipLocation    = $targetDir = 'C:\Program Files\Liberica'
-    Url64            = 'https://download.bell-sw.com/java/12.0.1/bellsoft-jdk12.0.1-windows-amd64.zip'
-    Checksum64       = '6E06B7416152EF3E1DC3BF170485ABC22F3D1DF6'
+    Url64            = 'https://download.bell-sw.com/java/12.0.2/bellsoft-jdk12.0.2-windows-amd64.zip'
+    Checksum64       = 'A6870289838B8529A3C3528395668A83705F6959'
     ChecksumType64   = 'sha1'
 }
 
 Install-ChocolateyZipPackage @packageArgs
-Install-ChocolateyEnvironmentVariable 'JAVA_HOME' $targetDir\jdk-12.0.1 'Machine'
+Install-ChocolateyEnvironmentVariable 'JAVA_HOME' $targetDir\jdk-12.0.2 'Machine'
 # The full path instead of the %JAVA_HOME% is needed so it can be removed with the Chocolatey Uninstall
-Install-ChocolateyPath 'C:\Program Files\Liberica\jdk-12.0.1\bin' -PathType 'Machine'
+Install-ChocolateyPath 'C:\Program Files\Liberica\jdk-12.0.2\bin' -PathType 'Machine'

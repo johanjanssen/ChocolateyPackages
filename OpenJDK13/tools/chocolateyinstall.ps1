ae2c9b9c31075ed2f0f0702f56469e3185fb8649
Johan Janssen <j.janssen@rhea.infosupport.net>
2019-09-16 19:51:31 +0200

    Added OpenJDK13.

---
+++ OpenJDK13/tools/chocolateyinstall.ps1
@@ -0,0 +1,12 @@
+﻿$packageArgs = @{
+    PackageName      = $env:ChocolateyPackageName
+    UnzipLocation    = $targetDir = 'C:\Program Files\OpenJDK'
+    Url64            = 'https://download.java.net/java/GA/jdk13/5b8a42f3905b406298b72d750b6919f6/33/GPL/openjdk-13_windows-x64_bin.zip'
+    Checksum64       = '053d8c87bb34347478512911a6218a389720bffcde4e496be5a54d51ad7c9c2f'
+    ChecksumType64   = 'sha256'
+}
+
+Install-ChocolateyZipPackage @packageArgs
+Install-ChocolateyEnvironmentVariable 'JAVA_HOME' $targetDir\jdk-13 'Machine'
+# The full path instead of the %JAVA_HOME% is needed so it can be removed with the Chocolatey Uninstall
+Install-ChocolateyPath 'C:\Program Files\OpenJDK\jdk-13\bin' -PathType 'Machine'

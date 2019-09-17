41271b992851102088d9b43c13e70deadc9188cf
Johan Janssen <j.janssen@rhea.infosupport.net>
2019-09-17 09:43:26 +0200

    Added package for Liberica Mission Control 7.0

---
+++ LibericaMissionControl/tools/chocolateyuninstall.ps1
@@ -0,0 +1,8 @@
+﻿$WildFlyDirectory = 'C:\Program Files\LibericaMissionControl' 
+
+rm -r 'C:\Program Files\LibericaMissionControl\lmc7.0'
+
+If ((Get-ChildItem -Force $WildFlyDirectory) -eq $Null) {
+    rmdir $WildFlyDirectory 
+}
+

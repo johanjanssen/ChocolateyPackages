28e2678306307072ba4e4cf1536d13f06b10f239
Johan Janssen <j.janssen@rhea.infosupport.net>
2019-09-19 10:07:39 +0200

    Fixed checksum and added SapMachine13 to the Travis build.

--- SapMachine/tools/chocolateyinstall.ps1
+++ SapMachine/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$packageArgs = @{
   PackageName = $env:ChocolateyPackageName
   Url64bit = 'https://github.com/SAP/SapMachine/releases/download/sapmachine-13/sapmachine-jdk-13_windows-x64_bin.msi'
-  Checksum64 = '3823d88d764cb301afa16c3c91cf98d226bf031ad174518fb01909f3c0ee1b05'
+  Checksum64 = '6B2437ACF6EB80F30C221432B4400118784CCCC57F70CA777B919BE049362FA3'
   ChecksumType64 = 'sha256'
   fileType      = 'msi'
   silentArgs    = "INSTALLLEVEL=3 /quiet"

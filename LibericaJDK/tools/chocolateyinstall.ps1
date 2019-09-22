3ac3507636f93154800c5cdc9b463a2c825bdf6f
Johan Janssen <j.janssen@rhea.infosupport.net>
2019-09-22 19:09:47 +0200

    Upgrade to LibericaJDK 13

--- LibericaJDK/tools/chocolateyinstall.ps1
+++ LibericaJDK/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$packageArgs = @{
   PackageName = $env:ChocolateyPackageName
-  Url64bit = 'https://download.bell-sw.com/java/12.0.2/bellsoft-jdk12.0.2-windows-amd64.msi'
-  Checksum64 = 'f80a78ab4a640a2a0c85ca3a72801ab99334cc04'
+  Url64bit = 'https://download.bell-sw.com/java/13/bellsoft-jdk13-windows-amd64.msi'
+  Checksum64 = '98fa8219320e9e90eae5aaba20cee3db588ec844'
   ChecksumType64 = 'sha1'
   fileType      = 'msi'
   silentArgs    = "INSTALLLEVEL=3 /quiet"

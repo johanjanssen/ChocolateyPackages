b6dfb462fc68364c62af36400b62d5e485c9561a
Johan Janssen <j.janssen@rhea.infosupport.net>
2019-10-08 09:11:08 +0200

    Removed uninstall, it's handled automatically by Chocolatey.

--- LibericaMissionControl/tools/chocolateyuninstall.ps1
+++
@@ -1,8 +0,0 @@
-﻿$MissionControlDirectory = 'C:\Program Files\LibericaMissionControl' 
-
-rm -r 'C:\Program Files\LibericaMissionControl\lmc7.0'
-
-If ((Get-ChildItem -Force $MissionControlDirectory) -eq $Null) {
-    rmdir $MissionControlDirectory 
-}
-

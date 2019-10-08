9e2b24a86d76a256d3b3028edb82195ead522d3c
Johan Janssen <j.janssen@rhea.infosupport.net>
2019-10-08 08:02:29 +0200

    Fixed review comments for Liberica Mission Control.

--- LibericaMissionControl/tools/chocolateyinstall.ps1
+++ LibericaMissionControl/tools/chocolateyinstall.ps1
@@ -1,9 +1,12 @@
-﻿$packageArgs = @{
+﻿# Path to the folder where the script is executing
+$toolsDir = (Split-Path -parent $MyInvocation.MyCommand.Definition)
+
+$packageArgs = @{
     PackageName      = $env:ChocolateyPackageName
-    UnzipLocation    = $targetDir = 'C:\Program Files\LibericaMissionControl'
-    Url              = 'http://download.bell-sw.com/lmc/bellsoft-lmc7.0-windows-amd64.zip'
-    Checksum         = 'e3694968827effbce19958747e3b69e08a1dda2a'
-    ChecksumType     = 'sha1'
+    UnzipLocation    = $toolsDir
+    Url64bit         = 'http://download.bell-sw.com/lmc/bellsoft-lmc7.0-windows-amd64.zip'
+    Checksum64       = 'e3694968827effbce19958747e3b69e08a1dda2a'
+    ChecksumType64   = 'sha1'
 }
 
 Install-ChocolateyZipPackage @packageArgs

d70ca9d1f3be70002c2c1d51ca85214d09471655
Johan Janssen <j.janssen@rhea.infosupport.net>
2019-10-08 08:20:32 +0200

    Upgrade to WildFly 18.0.0

--- WildFly/tools/chocolateyinstall.ps1
+++ WildFly/tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 ﻿$packageArgs = @{
     PackageName      = $env:ChocolateyPackageName
     UnzipLocation    = $targetDir = 'C:\Program Files\WildFly'
-    Url              = 'https://download.jboss.org/wildfly/17.0.1.Final/wildfly-17.0.1.Final.zip'
-    Checksum         = '6d3dd603eb3e177c6e7e06c649997ec182445789'
+    Url              = 'https://download.jboss.org/wildfly/18.0.0.Final/wildfly-18.0.0.Final.zip'
+    Checksum         = '28a1769ebf48faa44cbc77476fa98ceceb382d18'
     ChecksumType     = 'sha1'
 }
 
--- WildFly/tools/chocolateyuninstall.ps1
+++ WildFly/tools/chocolateyuninstall.ps1
@@ -1,6 +1,6 @@
 ﻿$WildFlyDirectory = 'C:\Program Files\WildFly' 
 
-rm -r 'C:\Program Files\WildFly\wildfly-17.0.1.Final'
+rm -r 'C:\Program Files\WildFly\wildfly-18.0.0.Final'
 
 If ((Get-ChildItem -Force $WildFlyDirectory) -eq $Null) {
     rmdir $WildFlyDirectory 

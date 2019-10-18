529950f42c9a272938e15c0d29de3e860ee92b7a
Johan Janssen <j.janssen@rhea.infosupport.net>
2019-10-18 08:30:32 +0200

    Upgraded Amazon Corretto 8 and 11.

--- Corretto11JDK/tools/chocolateyinstall.ps1
+++ Corretto11JDK/tools/chocolateyinstall.ps1
@@ -1,12 +1,10 @@
 ﻿$packageArgs = @{
-    PackageName      = $env:ChocolateyPackageName
-    UnzipLocation    = $targetDir = 'C:\Program Files\Corretto'
-    Url64            = 'https://d3pxv6yz143wms.cloudfront.net/11.0.4.11.1/amazon-corretto-11.0.4.11.1-windows-x64.zip'
-    Checksum64       = '707c839c3f56645454b8c8d31f255161'
-    ChecksumType64   = 'md5'
+  PackageName = $env:ChocolateyPackageName
+  Url64bit = 'https://d3pxv6yz143wms.cloudfront.net/11.0.5.10.1/amazon-corretto-11.0.5.10.1-windows-x64.msi'
+  Checksum64 = '836ac2d7e63a817436fbdbbc61921ae9'
+  ChecksumType64 = 'md5'
+  fileType      = 'msi'
+  silentArgs    = "INSTALLLEVEL=3 /quiet"
 }
 
-Install-ChocolateyZipPackage @packageArgs
-Install-ChocolateyEnvironmentVariable 'JAVA_HOME' $targetDir\jdk11.0.4_10 'Machine'
-# The full path instead of the %JAVA_HOME% is needed so it can be removed with the Chocolatey Uninstall
-Install-ChocolateyPath 'C:\Program Files\Corretto\jdk11.0.4_10\bin' -PathType 'Machine'
+Install-ChocolateyPackage @packageArgs
--- Corretto8JDK/tools/chocolateyinstall.ps1
+++ Corretto8JDK/tools/chocolateyinstall.ps1
@@ -1,12 +1,10 @@
 ﻿$packageArgs = @{
-    PackageName      = $env:ChocolateyPackageName
-    UnzipLocation    = $targetDir = 'C:\Program Files\Corretto'
-    Url64            = 'https://d3pxv6yz143wms.cloudfront.net/8.222.10.1/amazon-corretto-8.222.10.3-windows-x64-jdk.zip'
-    Checksum64       = '9879a7f69c0bd7d8c1bbb916df7b5f82'
-    ChecksumType64   = 'md5'
+  PackageName = $env:ChocolateyPackageName
+  Url64bit = 'https://d3pxv6yz143wms.cloudfront.net/8.232.09.1/amazon-corretto-8.232.09.1-windows-x86.msi'
+  Checksum64 = 'f94a12381f63284bcec017f2a9ebfe3c'
+  ChecksumType64 = 'md5'
+  fileType      = 'msi'
+  silentArgs    = "INSTALLLEVEL=3 /quiet"
 }
 
-Install-ChocolateyZipPackage @packageArgs
-Install-ChocolateyEnvironmentVariable 'JAVA_HOME' $targetDir\jdk1.8.0_222 'Machine'
-# The full path instead of the %JAVA_HOME% is needed so it can be removed with the Chocolatey Uninstall
-Install-ChocolateyPath 'C:\Program Files\Corretto\jdk1.8.0_222\bin' -PathType 'Machine'
+Install-ChocolateyPackage @packageArgs

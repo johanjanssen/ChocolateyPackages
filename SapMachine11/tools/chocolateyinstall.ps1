9ea61bb35af6a242a08afd3bb776277716a1ccb8
Johan Janssen <j.janssen@rhea.infosupport.net>
2019-10-18 08:54:31 +0200

    Updated LibericaJDK and SapMachine to the newest versions.

--- SapMachine11/tools/chocolateyinstall.ps1
+++ SapMachine11/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$packageArgs = @{
   PackageName = $env:ChocolateyPackageName
-  Url64bit = 'https://github.com/SAP/SapMachine/releases/download/sapmachine-11.0.4/sapmachine-jdk-11.0.4_windows-x64_bin.msi'
-  Checksum64 = '2884803CFCB1888ACAEC6005AB8F983762E4568052767FC3CD46970D17C9D82E'
+  Url64bit = 'https://github.com/SAP/SapMachine/releases/download/sapmachine-11.0.5/sapmachine-jdk-11.0.5_windows-x64_bin.msi'
+  Checksum64 = '4F009A62AF201CF8B3125772903C9C95993FB73E7A4A332DC8D35C35E78A80BF'
   ChecksumType64 = 'sha256'
   fileType      = 'msi'
   silentArgs    = "INSTALLLEVEL=3 /quiet"

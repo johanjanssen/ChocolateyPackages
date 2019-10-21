906cf646c19ff1b6f693b9cec2a82b27cf4f9fd3
Johan Janssen <j.janssen@rhea.infosupport.net>
2019-10-21 07:19:53 +0200

    Fixed bug and changed Corretto 8 x86 to x64

--- Corretto8JDK/tools/chocolateyinstall.ps1
+++ Corretto8JDK/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$packageArgs = @{
   PackageName = $env:ChocolateyPackageName
-  Url64bit = 'https://d3pxv6yz143wms.cloudfront.net/8.232.09.1/amazon-corretto-8.232.09.1-windows-x86.msi'
-  Checksum64 = 'f94a12381f63284bcec017f2a9ebfe3c'
+  Url64bit = 'https://d3pxv6yz143wms.cloudfront.net/8.232.09.1/amazon-corretto-8.232.09.1-windows-x64.msi'
+  Checksum64 = '620ee139aac5f05ab404006b5e33378f'
   ChecksumType64 = 'md5'
   fileType      = 'msi'
   silentArgs    = "INSTALLLEVEL=3 /quiet"

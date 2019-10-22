7c9ebc7a7eb5d24bfce36edea10c15e1a7221e77
Vasily Kopyl <vasily.kopyl@bell-sw.com>
2019-10-22 16:21:54 +0300

    Added Liberica 8 JDK

---
+++ Liberica8JDK/tools/chocolateyinstall.ps1
@@ -0,0 +1,10 @@
+﻿$packageArgs = @{
+  PackageName = $env:ChocolateyPackageName
+  Url64bit = 'https://download.bell-sw.com/java/8u232%2B10/bellsoft-jdk8u232%2B10-windows-amd64.msi'
+  Checksum64 = '9e53972758a3c21299329777c6895861b80df14c'
+  ChecksumType64 = 'sha1'
+  fileType      = 'msi'
+  silentArgs    = "INSTALLLEVEL=3 /quiet"
+}
+
+Install-ChocolateyPackage @packageArgs

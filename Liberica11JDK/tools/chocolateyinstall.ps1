dcb4af6fd5ee14570c097e538bb96b49281d016d
Vasily Kopyl <vasily.kopyl@bell-sw.com>
2019-10-22 16:22:32 +0300

    Changed URL to a newer version of 11.0.5

--- Liberica11JDK/tools/chocolateyinstall.ps1
+++ Liberica11JDK/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$packageArgs = @{
   PackageName = $env:ChocolateyPackageName
-  Url64bit = 'https://download.bell-sw.com/java/11.0.5/bellsoft-jdk11.0.5-windows-amd64.msi'
-  Checksum64 = 'e22d218b600aa6ebb8f4c76a35411870d0b52e3c'
+  Url64bit = 'https://download.bell-sw.com/java/11.0.5%2B11/bellsoft-jdk11.0.5%2B11-windows-amd64.msi'
+  Checksum64 = 'd68f33295bb6bd9817ebb3e32399497babf4a460'
   ChecksumType64 = 'sha1'
   fileType      = 'msi'
   silentArgs    = "INSTALLLEVEL=3 /quiet"

e3d1d580b05e4d17a74b1a96434127345db368c6
Johan Janssen <j.janssen@rhea.infosupport.net>
2019-10-24 07:23:36 +0200

    Upgraded ojdkbuild to 8.232, 11.0.5 and 13.0.1

--- ojdkbuild/tools/chocolateyinstall.ps1
+++ ojdkbuild/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$packageArgs = @{
   PackageName = $env:ChocolateyPackageName
-  Url64bit = 'https://github.com/ojdkbuild/ojdkbuild/releases/download/java-12-openjdk-12.0.2.9-1/java-12-openjdk-12.0.2.9-1.windows.ojdkbuild.x86_64.msi'
-  Checksum64 = '22db792f00f376e31fca5da6dd7b74d02b55fc1d7f36d8abe48783d0a2a5b5f8'
+  Url64bit = 'https://github.com/ojdkbuild/ojdkbuild/releases/download/java-13-openjdk-13.0.1.9-1/java-13-openjdk-13.0.1.9-1.windows.ojdkbuild.x86_64.msi'
+  Checksum64 = 'e96164ee983f296b92482be5dcbd2a3d45260612f4dfb20438d1f0b62e21f8ff'
   ChecksumType64 = 'sha256'
   fileType      = 'msi'
   silentArgs    = "INSTALLLEVEL=3 /quiet"

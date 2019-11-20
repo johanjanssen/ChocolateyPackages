8d2ddfb9aa36cad53ff1a4578d5b077b92e367fb
Johan Janssen <j.janssen@rhea.infosupport.net>
2019-11-20 20:05:58 +0100

    Upgraded package to GraalVM 19.3.0 with Java 11 and added Java 8 and Java 11 specific packages.

---
+++ GraalVMJava8/tools/chocolateyinstall.ps1
@@ -0,0 +1,12 @@
+﻿$packageArgs = @{
+    PackageName      = $env:ChocolateyPackageName
+    UnzipLocation    = $targetDir = 'C:\Program Files\GraalVM'
+    Url64            = 'https://github.com/graalvm/graalvm-ce-builds/releases/download/vm-19.3.0/graalvm-ce-java8-windows-amd64-19.3.0.zip'
+    Checksum64       = 'EB0F000A074C7C7541E1EEBB06AE5DC86A5AEFB8A050949CE8F17FA5D1EEE13C'
+    ChecksumType64   = 'sha256'
+}
+
+Install-ChocolateyZipPackage @packageArgs
+Install-ChocolateyEnvironmentVariable 'JAVA_HOME' $targetDir\graalvm-ce-java8-19.3.0 'Machine'
+# The full path instead of the %JAVA_HOME% is needed so it can be removed with the Chocolatey Uninstall
+Install-ChocolateyPath 'C:\Program Files\GraalVM\graalvm-ce-java8-19.3.0\bin' -PathType 'Machine'

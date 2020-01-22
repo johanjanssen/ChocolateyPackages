5e29a1c4bd9384a06d08fa964c49657d980ecd89
johanjanssen <johanjanssen2001@gmail.com>
2020-01-22 19:43:29 +0100

    Fixed SapMachine 13 hashes.

--- SapMachine/tools/chocolateyinstall.ps1
+++ SapMachine/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$packageArgs = @{
   PackageName = $env:ChocolateyPackageName
   Url64bit = 'https://github.com/SAP/SapMachine/releases/download/sapmachine-13.0.2/sapmachine-jdk-13.0.2_windows-x64_bin.msi'
-  Checksum64 = 'b92730ec4a0bafa33957a57c5b55ee77ab5a0e09347539a34d94326a142ae863'
+  Checksum64 = 'E074909CED3952B12D2636C22D5E9E4AB4E55A608E4C5C3BA5F6003FE5BDCE47'
   ChecksumType64 = 'sha256'
   fileType      = 'msi'
   silentArgs    = "INSTALLLEVEL=3 /quiet"

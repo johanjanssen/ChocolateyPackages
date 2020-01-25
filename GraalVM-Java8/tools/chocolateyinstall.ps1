66efcee06f89e6f09bfe17f30ad12a9c26070a3d
johanjanssen <johanjanssen2001@gmail.com>
2020-01-25 13:34:35 +0100

    Removed hardcoded Program Files directory on c disk.

--- GraalVM-Java8/tools/chocolateyinstall.ps1
+++ GraalVM-Java8/tools/chocolateyinstall.ps1
@@ -1,6 +1,9 @@
-﻿$packageArgs = @{
+﻿$programFiles = (${env:ProgramFiles}, ${env:ProgramFiles(x86)} -ne $null)[0]
+$installDir = "$programFiles\GraalVM"
+
+$packageArgs = @{
     PackageName      = $env:ChocolateyPackageName
-    UnzipLocation    = $targetDir = 'C:\Program Files\GraalVM'
+    UnzipLocation    = $targetDir = $installDir
     Url64            = 'https://github.com/graalvm/graalvm-ce-builds/releases/download/vm-19.3.1/graalvm-ce-java8-windows-amd64-19.3.1.zip'
     Checksum64       = '77EB35E88EEE297BBAA63CA9856379D0EC31953370C8EA5168C8EE091B656A9F'
     ChecksumType64   = 'sha256'
@@ -9,4 +12,4 @@
 Install-ChocolateyZipPackage @packageArgs
 Install-ChocolateyEnvironmentVariable 'JAVA_HOME' $targetDir\graalvm-ce-java8-19.3.1 'Machine'
 # The full path instead of the %JAVA_HOME% is needed so it can be removed with the Chocolatey Uninstall
-Install-ChocolateyPath 'C:\Program Files\GraalVM\graalvm-ce-java8-19.3.1\bin' -PathType 'Machine'
+Install-ChocolateyPath $targetDir\graalvm-ce-java8-19.3.1\bin -PathType 'Machine'

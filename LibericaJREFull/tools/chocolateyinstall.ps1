f933a43e9314d2162791ae535bcba39e0aee1971
johanjanssen <johanjanssen2001@gmail.com>
2020-03-19 08:34:49 +0100

    Added Liberica JDK and JRE lite and full versions.

---
+++ LibericaJREFull/tools/chocolateyinstall.ps1
@@ -0,0 +1,10 @@
+﻿$packageArgs = @{
+  PackageName = $env:ChocolateyPackageName
+  Url64bit = 'http://download.bell-sw.com/java/13.0.2+9/bellsoft-jre13.0.2+9-windows-amd64-full.msi'
+  Checksum64 = '8476D3AB474A54FF8DDCBCF15A842B10CFAA6F29'
+  ChecksumType64 = 'sha1'
+  fileType      = 'msi'
+  silentArgs    = "INSTALLLEVEL=3 /quiet"
+}
+
+Install-ChocolateyPackage @packageArgs

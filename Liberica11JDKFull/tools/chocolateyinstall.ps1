eff70a108725d7664ac17d39f5685164fa232ab1
johanjanssen <johanjanssen2001@gmail.com>
2020-03-20 16:25:15 +0100

    Added Liberica 11 Full JDK and JRE.

---
+++ Liberica11JDKFull/tools/chocolateyinstall.ps1
@@ -0,0 +1,10 @@
+﻿$packageArgs = @{
+  PackageName = $env:ChocolateyPackageName
+  Url64bit = 'http://download.bell-sw.com/java/11.0.6+10/bellsoft-jdk11.0.6+10-windows-amd64-full.msi'
+  Checksum64 = 'B7087A8DE5676008298392CA267BC2474073E4DE'
+  ChecksumType64 = 'sha1'
+  fileType      = 'msi'
+  silentArgs    = "INSTALLLEVEL=3 /quiet"
+}
+
+Install-ChocolateyPackage @packageArgs
---
+++ Liberica11JREFull/tools/chocolateyinstall.ps1
@@ -0,0 +1,10 @@
+﻿$packageArgs = @{
+  PackageName = $env:ChocolateyPackageName
+  Url64bit = 'http://download.bell-sw.com/java/11.0.6+10/bellsoft-jre11.0.6+10-windows-amd64-full.msi'
+  Checksum64 = '3A147FFBADC47C1392EC76F6F14DC8679249D06A'
+  ChecksumType64 = 'sha1'
+  fileType      = 'msi'
+  silentArgs    = "INSTALLLEVEL=3 /quiet"
+}
+
+Install-ChocolateyPackage @packageArgs

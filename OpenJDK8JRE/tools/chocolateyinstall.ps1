08335e500655edc84edd3c367331bf4e0c900db6
johanjanssen <johanjanssen2001@gmail.com>
2020-04-15 09:19:42 +0200

    Fixed the checksum for OpenJDK 8 JRE.

--- OpenJDK8JRE/tools/chocolateyinstall.ps1
+++ OpenJDK8JRE/tools/chocolateyinstall.ps1
@@ -5,7 +5,7 @@ $packageArgs = @{
     PackageName      = $env:ChocolateyPackageName
     UnzipLocation    = $targetDir = $installDir
     Url64            = 'https://github.com/AdoptOpenJDK/openjdk8-upstream-binaries/releases/download/jdk8u252-b09/OpenJDK8U-jre_x64_windows_8u252b09.zip'
-    Checksum64       = '019E7AD1481126470D6CBAA1729A92C2AF7A85A23BF6969CCD838729196C9FE5'
+    Checksum64       = '1606284381728AD3F456DB4C83A56E49BE195C45520ED4288AE770AF7AD8AEB9'
     ChecksumType64   = 'sha256'
 }
 

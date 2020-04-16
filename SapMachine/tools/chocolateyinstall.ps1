26122ab980c73fe51d48a1dd81323e88b804a765
johanjanssen <johanjanssen2001@gmail.com>
2020-04-16 20:09:27 +0200

    Updated SapMachine.

--- SapMachine/tools/chocolateyinstall.ps1
+++ SapMachine/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$packageArgs = @{
   PackageName = $env:ChocolateyPackageName
-  Url64bit = 'https://github.com/SAP/SapMachine/releases/download/sapmachine-14/sapmachine-jdk-14_windows-x64_bin.msi'
-  Checksum64 = '77C5B352E27C04DA13138C7AC3E62CAA3761D6B57642C8CA79CE12FE752C3540'
+  Url64bit = 'https://github.com/SAP/SapMachine/releases/download/sapmachine-14.0.1/sapmachine-jdk-14.0.1_windows-x64_bin.msi'
+  Checksum64 = 'E240F33490F8BB8211C1EC39C6078282AEBA0A3C087627193BE2DE6774AE84AD'
   ChecksumType64 = 'sha256'
   fileType      = 'msi'
   silentArgs    = "INSTALLLEVEL=3 /quiet"
--- SapMachineJRE/tools/chocolateyinstall.ps1
+++ SapMachineJRE/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$packageArgs = @{
   PackageName = $env:ChocolateyPackageName
-  Url64bit = 'https://github.com/SAP/SapMachine/releases/download/sapmachine-14/sapmachine-jre-14_windows-x64_bin.msi'
-  Checksum64 = 'BA41703BEE51AD907B8FC28992F0FCB2DD90FFB2ED09BCC95CDE2D369FF5410B'
+  Url64bit = 'https://github.com/SAP/SapMachine/releases/download/sapmachine-14.0.1/sapmachine-jre-14.0.1_windows-x64_bin.msi'
+  Checksum64 = 'D321C859BB4ECABCFE4016DF988566679D397137ECCCF1C11F9A859FFE176C84'
   ChecksumType64 = 'sha256'
   fileType      = 'msi'
   silentArgs    = "INSTALLLEVEL=3 /quiet"

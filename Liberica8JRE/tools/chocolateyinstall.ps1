49c4485529b28f72db2cbe911e8a6a8fe87b1e6a
johanjanssen <johanjanssen2001@gmail.com>
2020-04-20 14:08:07 +0200

    Fixed checksum

--- Liberica8JRE/tools/chocolateyinstall.ps1
+++ Liberica8JRE/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$packageArgs = @{
   PackageName = $env:ChocolateyPackageName
   Url64bit = 'https://download.bell-sw.com/java/8u252+9/bellsoft-jre8u252+9-windows-amd64.msi'
-  Checksum64 = '85F5C11DEE0CBF3C65FAA5E48394B47CF9174C75'
+  Checksum64 = '2B382FD802B11C837DCBE1D99DDBA5FAC1625AE3'
   ChecksumType64 = 'sha1'
   fileType      = 'msi'
   silentArgs    = "INSTALLLEVEL=3 /quiet"

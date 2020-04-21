a9774bfe2abeac1ac99e024e8d4fa3e86ab44efd
johanjanssen <johanjanssen2001@gmail.com>
2020-04-21 08:40:58 +0200

    Ojdkbuild updates.

--- ojdkbuild/tools/chocolateyinstall.ps1
+++ ojdkbuild/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$packageArgs = @{
   PackageName = $env:ChocolateyPackageName
-  Url64bit = 'https://github.com/ojdkbuild/ojdkbuild/releases/download/java-13-openjdk-13.0.2.8-1/java-13-openjdk-13.0.2.8-1.windows.ojdkbuild.x86_64.msi'
-  Checksum64 = 'a5de12f580df247963ba535469fcb86d84a1a1a68c1d89c0c7d9e4944a75ac78'
+  Url64bit = 'https://github.com/ojdkbuild/ojdkbuild/releases/download/java-14-openjdk-14.0.1.7-0/java-14-openjdk-14.0.1.7-1.windows.ojdkbuild.x86_64.msi'
+  Checksum64 = '3ee6ec20f472bcc9608153b659d63320a0471968d756a9e7d9e5997c3c580e7f'
   ChecksumType64 = 'sha256'
   fileType      = 'msi'
   silentArgs    = "INSTALLLEVEL=3 /quiet"

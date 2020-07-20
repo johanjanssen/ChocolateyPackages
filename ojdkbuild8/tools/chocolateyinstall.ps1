deb1d4235051050ced6ff2f9e6721b1c82a25be0
johanjanssen <johanjanssen2001@gmail.com>
2020-07-20 14:58:19 +0200

    Updated ojdkbuild to 8.0.262, 11.0.8, 14.0.2.

--- ojdkbuild8/tools/chocolateyinstall.ps1
+++ ojdkbuild8/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$packageArgs = @{
   PackageName = $env:ChocolateyPackageName
-  Url64bit = 'https://github.com/ojdkbuild/ojdkbuild/releases/download/java-1.8.0-openjdk-1.8.0.252-1.b09/java-1.8.0-openjdk-1.8.0.252-1.b09.ojdkbuild.windows.x86_64.msi'
-  Checksum64 = 'a3643ab85c1a2f6db1501b8216f6e944236781bafb7e8943eb621e06f42d50ad'
+  Url64bit = 'https://github.com/ojdkbuild/ojdkbuild/releases/download/java-1.8.0-openjdk-1.8.0.262-1.b10/java-1.8.0-openjdk-1.8.0.262-1.b10.ojdkbuild.windows.x86_64.msi'
+  Checksum64 = '8d012d3e75adac829c8e1f151cd34520a05d66cec8871f195c76873e40ecbc63'
   ChecksumType64 = 'sha256'
   fileType      = 'msi'
   silentArgs    = "INSTALLLEVEL=3 /quiet"

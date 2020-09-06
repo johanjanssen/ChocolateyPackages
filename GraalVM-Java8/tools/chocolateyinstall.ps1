4d4aea1abca35eab2089574aec0ccd02c71714c8
johanjanssen <johanjanssen2001@gmail.com>
2020-09-06 18:09:51 +0200

    Update GraalVM to 20.2.0

--- GraalVM-Java8/tools/chocolateyinstall.ps1
+++ GraalVM-Java8/tools/chocolateyinstall.ps1
@@ -4,12 +4,12 @@ $installDir = "$programFiles\GraalVM"
 $packageArgs = @{
     PackageName      = $env:ChocolateyPackageName
     UnzipLocation    = $targetDir = $installDir
-    Url64            = 'https://github.com/graalvm/graalvm-ce-builds/releases/download/vm-20.1.0/graalvm-ce-java8-windows-amd64-20.1.0.zip'
-    Checksum64       = 'A0A5899BDE08DCF06408A38A12FECA62FB929DE06D54550A07BBE651F0EB20B3'
+    Url64            = 'https://github.com/graalvm/graalvm-ce-builds/releases/download/vm-20.2.0/graalvm-ce-java8-windows-amd64-20.2.0.zip'
+    Checksum64       = '9DBF11B3F7F54D6FDBDFED4A1D141447F8B576A6BC1C6AD0BFDDE42D15452D9B'
     ChecksumType64   = 'sha256'
 }
 
 Install-ChocolateyZipPackage @packageArgs
-Install-ChocolateyEnvironmentVariable 'JAVA_HOME' $targetDir\graalvm-ce-java8-20.1.0 'Machine'
+Install-ChocolateyEnvironmentVariable 'JAVA_HOME' $targetDir\graalvm-ce-java8-20.2.0 'Machine'
 # The full path instead of the %JAVA_HOME% is needed so it can be removed with the Chocolatey Uninstall
-Install-ChocolateyPath $targetDir\graalvm-ce-java8-20.1.0\bin -PathType 'Machine'
+Install-ChocolateyPath $targetDir\graalvm-ce-java8-20.2.0\bin -PathType 'Machine'

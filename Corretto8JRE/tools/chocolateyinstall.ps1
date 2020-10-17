ec234ded125c8d5c2101fbd4c87fe36bf76eae28
johanjanssen <johanjanssen2001@gmail.com>
2020-10-17 10:11:43 +0200

    Upgraded Corretto to 8.265.01.1

--- Corretto8JRE/tools/chocolateyinstall.ps1
+++ Corretto8JRE/tools/chocolateyinstall.ps1
@@ -4,8 +4,8 @@ $installDir = "$programFiles\Corretto"
 $packageArgs = @{
     PackageName      = $env:ChocolateyPackageName
     UnzipLocation    = $targetDir = $installDir
-    Url64            = 'https://corretto.aws/downloads/resources/8.262.10.1/amazon-corretto-8.262.10.1-windows-x64-jre.zip'
-    Checksum64       = '5ab1591c50a9c8e7ada999f81864da9f'
+    Url64            = 'https://corretto.aws/downloads/resources/8.265.01.1/amazon-corretto-8.265.01.1-windows-x64-jre.zip'
+    Checksum64       = 'b82a1dd6249e9ccfada3f73dd29f08f3'
     ChecksumType64   = 'md5'
 }
 

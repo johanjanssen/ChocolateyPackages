206e3f16715f6009a3b58a8c368d59c39d240391
johanjanssen <johanjanssen2001@gmail.com>
2021-01-28 07:49:24 +0100

    Fixed removed ojdkbuild 8 url.

--- ojdkbuild8/tools/chocolateyinstall.ps1
+++ ojdkbuild8/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$packageArgs = @{
   PackageName = $env:ChocolateyPackageName
-  Url64bit = 'https://github.com/ojdkbuild/ojdkbuild/releases/download/java-1.8.0-openjdk-1.8.0.282-1.b07.ea-x86/java-1.8.0-openjdk-1.8.0.282-1.b08.ojdkbuild.windows.x86.msi'
-  Checksum64 = 'd3f835024aeaef28c5fe2ab88fc350e08a00d90c77dda5aef1f54d78799cee2b'
+  Url64bit = 'https://github.com/ojdkbuild/ojdkbuild/releases/download/java-1.8.0-openjdk-1.8.0.282-1.b08/java-1.8.0-openjdk-1.8.0.282-1.b08.ojdkbuild.windows.x86_64.msi'
+  Checksum64 = '58b5d4488f5038be791885891a2b1230a227c74bc9aa6e043cd3880427b7bbf9'
   ChecksumType64 = 'sha256'
   fileType      = 'msi'
   silentArgs    = "INSTALLLEVEL=3 /quiet"

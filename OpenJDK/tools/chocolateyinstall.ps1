64e5b3513202de44a11a6ac189a4e9c7d17fe09d
johanjanssen <johanjanssen2001@gmail.com>
2021-03-16 18:26:43 +0100

    OpenJDK and SapMachine 16, WildFly 23 and GraalVM 21.0.0.2

--- OpenJDK/tools/chocolateyinstall.ps1
+++ OpenJDK/tools/chocolateyinstall.ps1
@@ -4,12 +4,12 @@ $installDir = "$programFiles\OpenJDK"
 $packageArgs = @{
     PackageName      = $env:ChocolateyPackageName
     UnzipLocation    = $targetDir = $installDir
-    Url64            = 'https://download.java.net/java/GA/jdk15.0.2/0d1cfde4252546c6931946de8db48ee2/7/GPL/openjdk-15.0.2_windows-x64_bin.zip'
-    Checksum64       = 'ecbe7f32bc6bff2b6c8e9b68f19cbf4ddf54a492c918ba471f32d645cf1c5cf4'
+    Url64            = 'https://download.java.net/java/GA/jdk16/7863447f0ab643c585b9bdebf67c69db/36/GPL/openjdk-16_windows-x64_bin.zip'
+    Checksum64       = 'a78bdeaad186297601edac6772d931224d7af6f682a43372e693c37020bd37d6'
     ChecksumType64   = 'sha256'
 }
 
 Install-ChocolateyZipPackage @packageArgs
-Install-ChocolateyEnvironmentVariable 'JAVA_HOME' $targetDir\jdk-15.0.2 'Machine'
+Install-ChocolateyEnvironmentVariable 'JAVA_HOME' $targetDir\jdk-16 'Machine'
 # The full path instead of the %JAVA_HOME% is needed so it can be removed with the Chocolatey Uninstall
-Install-ChocolateyPath $targetDir\jdk-15.0.2\bin -PathType 'Machine'
+Install-ChocolateyPath $targetDir\jdk-16\bin -PathType 'Machine'

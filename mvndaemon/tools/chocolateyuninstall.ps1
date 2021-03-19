4744463cf831fdfbefd4c6cc8b078810598408a6
johanjanssen <johanjanssen2001@gmail.com>
2021-03-19 12:01:09 +0100

    Maven Daemon 0.4.3

--- mvndaemon/tools/chocolateyuninstall.ps1
+++ mvndaemon/tools/chocolateyuninstall.ps1
@@ -1,4 +1,4 @@
 ﻿$programFiles = (${env:ProgramFiles}, ${env:ProgramFiles(x86)} -ne $null)[0]
 $installDir = "$programFiles\mvndaemon"
 
-rm -r "$installDir\mvnd-0.4.2-windows-amd64"
+rm -r "$installDir\mvnd-0.4.3-windows-amd64"

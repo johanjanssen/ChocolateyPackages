cc0c359b092507d05aa847640d1ffb9cd9b900e8
johanjanssen <johanjanssen2001@gmail.com>
2021-04-21 22:54:21 +0200

    Fixed build issues.

--- Liberica11JDK/tools/chocolateyinstall.ps1
+++ Liberica11JDK/tools/chocolateyinstall.ps1
@@ -1,6 +1,6 @@
 ﻿$packageArgs = @{
   PackageName = $env:ChocolateyPackageName
-  Url64bit = 'hhttps://download.bell-sw.com/java/11.0.11+9/bellsoft-jdk11.0.11+9-windows-amd64.msi'
+  Url64bit = 'https://download.bell-sw.com/java/11.0.11+9/bellsoft-jdk11.0.11+9-windows-amd64.msi'
   Checksum64 = 'f2cde861db4262577ff1dc2ce82c388b3ecfaad5'
   ChecksumType64 = 'sha1'
   fileType      = 'msi'
--- OpenJDK/tools/chocolateyinstall.ps1
+++ OpenJDK/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $installDir = "$programFiles\OpenJDK"
 $packageArgs = @{
     PackageName      = $env:ChocolateyPackageName
     UnzipLocation    = $targetDir = $installDir
-    Url64            = 'https://download.java.net/java/GA/jdk16.0.1/7147401fd7354114ac51ef3e1328291f/9/GPL/openjdk-16.0.1.0.1_windows-x64_bin.zip'
+    Url64            = 'https://download.java.net/java/GA/jdk16.0.1/7147401fd7354114ac51ef3e1328291f/9/GPL/openjdk-16.0.1_windows-x64_bin.zip'
     Checksum64       = '733b45b09463c97133d70c2368f1b9505da58e88f2c8a84358dd4accfd06a7a4'
     ChecksumType64   = 'sha256'
 }

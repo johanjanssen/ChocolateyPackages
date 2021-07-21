b8b40d49915d76fccf2a295eaadf48b49a2ef778
johanjanssen <johanjanssen2001@gmail.com>
2021-07-21 13:42:18 +0200

    Fixed hash for Liberica 8 JRE.

--- Liberica8JRE/tools/chocolateyinstall.ps1
+++ Liberica8JRE/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$packageArgs = @{
   PackageName = $env:ChocolateyPackageName
   Url64bit = 'https://download.bell-sw.com/java/8u302+8/bellsoft-jre8u302+8-windows-amd64.msi'
-  Checksum64 = '7a197aaaec2d9d63dc13ae4fdbe113d78d3fd5de'
+  Checksum64 = '386bc0c4a6817c195d1e7f4b427c7692500d5870'
   ChecksumType64 = 'sha1'
   fileType      = 'msi'
   silentArgs    = "INSTALLLEVEL=3 /quiet"

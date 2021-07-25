32ab5612d4acda36fd98621704d3f5a3eeebe5a6
johanjanssen <johanjanssen2001@gmail.com>
2021-07-25 10:24:36 +0200

    Upgrade to 8.0.302, 11.0.12 and 16.0.2

--- ojdkbuild11/tools/chocolateyinstall.ps1
+++ ojdkbuild11/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$packageArgs = @{
   PackageName = $env:ChocolateyPackageName
-  Url64bit = 'https://github.com/ojdkbuild/ojdkbuild/releases/download/java-11-openjdk-11.0.11.9-1/java-11-openjdk-11.0.11.9-1.windows.ojdkbuild.x86_64.msi'
-  Checksum64 = 'deccb2a103eb8e562daaa69e9db69be76ff6c05bdab795dd8e0a53ac8be8a6fa'
+  Url64bit = 'https://github.com/ojdkbuild/ojdkbuild/releases/download/java-11-openjdk-11.0.12.7-1/java-11-openjdk-11.0.12.7-1.windows.ojdkbuild.x86_64.msi'
+  Checksum64 = '975fe76ce3e821251313e9a17e248f74a875e1d8bff40f026b4573062bae2a10'
   ChecksumType64 = 'sha256'
   fileType      = 'msi'
   silentArgs    = "INSTALLLEVEL=3 /quiet"

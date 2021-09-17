0501a1c5308a1fc6252d930b62b9a49d0c6b13b5
johanjanssen <johanjanssen2001@gmail.com>
2021-09-17 12:46:42 +0200

    Added Oracle JDK 17

---
+++ OracleJDK/tools/chocolateyinstall.ps1
@@ -0,0 +1,10 @@
+﻿$packageArgs = @{
+  PackageName = $env:ChocolateyPackageName
+  Url64bit = 'https://download.oracle.com/java/17/archive/jdk-17_windows-x64_bin.msi'
+  Checksum64 = 'b59d7216e11e438179dd716404d9bbb45fdd11714cccf553b857cc90c525ebc3'
+  ChecksumType64 = 'sha256'
+  fileType      = 'msi'
+  silentArgs    = "INSTALLLEVEL=3 /quiet"
+}
+
+Install-ChocolateyPackage @packageArgs

b86eca1f145c9bee87a9b536c2ff7b76df187a79
johanjanssen <johanjanssen2001@gmail.com>
2021-10-26 12:48:11 +0200

    Upgrades to 8.312 11.0.13 17.0.1

---
+++ GraalVM-Java17/tools/chocolateyinstall.ps1
@@ -0,0 +1,15 @@
+﻿$programFiles = (${env:ProgramFiles}, ${env:ProgramFiles(x86)} -ne $null)[0]
+$installDir = "$programFiles\GraalVM"
+
+$packageArgs = @{
+    PackageName      = $env:ChocolateyPackageName
+    UnzipLocation    = $targetDir = $installDir
+    Url64            = 'https://github.com/graalvm/graalvm-ce-builds/releases/download/vm-21.3.0/graalvm-ce-java17-windows-amd64-21.3.0.zip'
+    Checksum64       = 'dd8b89ad60bf909327fdb6e5711d944c5e581d47add6414757ac9b293febc7be'
+    ChecksumType64   = 'sha256'
+}
+
+Install-ChocolateyZipPackage @packageArgs
+Install-ChocolateyEnvironmentVariable 'JAVA_HOME' $targetDir\graalvm-ce-java17-21.3.0 'Machine'
+# The full path instead of the %JAVA_HOME% is needed so it can be removed with the Chocolatey Uninstall
+Install-ChocolateyPath $targetDir\graalvm-ce-java17-21.3.0\bin -PathType 'Machine'

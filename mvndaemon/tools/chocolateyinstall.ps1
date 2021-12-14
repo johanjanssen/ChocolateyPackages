3ace7de653c4d2d9138cc9a57ad04d59f51a371f
johanjanssen <johanjanssen2001@gmail.com>
2021-12-14 11:54:52 +0100

    Upgrade of mvnd to 0.7.1

--- mvndaemon/tools/chocolateyinstall.ps1
+++ mvndaemon/tools/chocolateyinstall.ps1
@@ -4,10 +4,10 @@ $installDir = "$programFiles\mvndaemon"
 $packageArgs = @{
     PackageName      = $env:ChocolateyPackageName
     UnzipLocation    = $targetDir = $installDir
-    Url64            = 'https://github.com/mvndaemon/mvnd/releases/download/0.7.0/mvnd-0.7.0-windows-amd64.zip'
-    Checksum64       = '43d80dbf45c13bdef2cda7d3f4e94b4058c8239b75b6e251dc685de07798455b'
+    Url64            = 'https://github.com/mvndaemon/mvnd/releases/download/0.7.1/mvnd-0.7.1-windows-amd64.zip'
+    Checksum64       = '749f647df21977e7ab8f1296ae39dedf1067aaaf173eddfb685b73e01d8352ff'
     ChecksumType64   = 'sha256'
 }
 
 Install-ChocolateyZipPackage @packageArgs
-Install-ChocolateyPath $targetDir\mvnd-0.7.0-windows-amd64\bin -PathType 'Machine'
+Install-ChocolateyPath $targetDir\mvnd-0.7.1-windows-amd64\bin -PathType 'Machine'

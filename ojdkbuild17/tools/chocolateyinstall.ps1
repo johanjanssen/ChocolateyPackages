0689a0f0a4b5f47d777e256b749aec7688e52158
johanjanssen <johanjanssen2001@gmail.com>
2022-01-19 20:11:43 +0100

    Upgrades to 17.0.2, 11.0.14 and 8.322.

---
+++ ojdkbuild17/tools/chocolateyinstall.ps1
@@ -0,0 +1,10 @@
+﻿$packageArgs = @{
+  PackageName = $env:ChocolateyPackageName
+  Url64bit = 'https://github.com/ojdkbuild/ojdkbuild/releases/download/java-17-openjdk-17.0.1.0.12-1/java-17-openjdk-17.0.1.0.12-1.win.x86_64.msi'
+  Checksum64 = '58013158d4b8f8906feb5875a861f910fcc41236acde8d94f899f26581151b7e'
+  ChecksumType64 = 'sha256'
+  fileType      = 'msi'
+  silentArgs    = "INSTALLLEVEL=3 /quiet"
+}
+
+Install-ChocolateyPackage @packageArgs

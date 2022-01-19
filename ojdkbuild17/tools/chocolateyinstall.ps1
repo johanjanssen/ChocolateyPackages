eeafa2eccd2d39ceb7dcd573f28739d00b7c9abd
johanjanssen <johanjanssen2001@gmail.com>
2022-01-19 20:17:30 +0100

    Fixed checksum for ojdkbuild17

--- ojdkbuild17/tools/chocolateyinstall.ps1
+++ ojdkbuild17/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$packageArgs = @{
   PackageName = $env:ChocolateyPackageName
   Url64bit = 'https://github.com/ojdkbuild/ojdkbuild/releases/download/java-17-openjdk-17.0.1.0.12-1/java-17-openjdk-17.0.1.0.12-1.win.x86_64.msi'
-  Checksum64 = '58013158d4b8f8906feb5875a861f910fcc41236acde8d94f899f26581151b7e'
+  Checksum64 = '20c2e50b615a19a670d0e069816c0ecc616d6f8924f44a67b6d0cf7447f2c432'
   ChecksumType64 = 'sha256'
   fileType      = 'msi'
   silentArgs    = "INSTALLLEVEL=3 /quiet"

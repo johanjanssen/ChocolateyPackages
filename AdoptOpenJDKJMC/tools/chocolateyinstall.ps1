a8670dc457aaa142e7c5ef7331f517ad5856066e
Pranay Kanwar <pranay.kanwar@gmail.com>
2022-03-30 13:59:26 +0530

    Upgrade JMC to 8.2.0

--- AdoptOpenJDKJMC/tools/chocolateyinstall.ps1
+++ AdoptOpenJDKJMC/tools/chocolateyinstall.ps1
@@ -4,8 +4,8 @@ $toolsDir = (Split-Path -parent $MyInvocation.MyCommand.Definition)
 $packageArgs = @{
     PackageName      = $env:ChocolateyPackageName
     UnzipLocation    = $toolsDir
-    Url64bit         = 'https://github.com/AdoptOpenJDK/openjdk-jmc-overrides/releases/download/8.1.0/org.openjdk.jmc-8.1.0-win32.win32.x86_64.zip'
-    Checksum64       = 'A9C433DF56BDE62FF491D572151B280A5ABDACB1FF4A7B89CB0C14834C39EB50'
+    Url64bit         = 'https://github.com/adoptium/jmc-build/releases/download/8.2.0/org.openjdk.jmc-8.2.0-win32.win32.x86_64.zip'
+    Checksum64       = '8DF95DD0060B30A7AC455DB41C03967D1438D8A5353095B14FA81FBC9E489F3C'
     ChecksumType64   = 'sha256'
 }
 

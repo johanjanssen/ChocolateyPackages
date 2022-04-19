ef4c95b5c4e545482b9a08bf292c5c38bd170de1
johanjanssen <johanjanssen@users.noreply.github.com>
2022-04-19 19:38:48 +0000

    [create-pull-request] automated change

--- Corretto11JDK/tools/chocolateyinstall.ps1
+++ Corretto11JDK/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$packageArgs = @{
   PackageName = $env:ChocolateyPackageName
-  Url64bit = 'https://corretto.aws/downloads/resources/11.0.14.10.1/amazon-corretto-11.0.14.10.1-windows-x64.msi'
-  Checksum64 = 'bf5c6337840f3a0a043d2ae68923575b'
+  Url64bit = 'https://corretto.aws/downloads/resources/11.0.15.9.1/amazon-corretto-11.0.15.9.1-windows-x64.msi'
+  Checksum64 = '358d13e4490f88f97e9eddba8c1e3f31'
   ChecksumType64 = 'md5'
   fileType      = 'msi'
   silentArgs    = "INSTALLLEVEL=3 /quiet"

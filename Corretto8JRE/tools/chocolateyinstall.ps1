01f72b82e1b56a8c5c5cb73335c36619812e7d2d
Johan Janssen <johanjanssen2001@gmail.com>
2022-04-20 22:03:34 +0200

    Upgraded Corretto JRE to 8.332

--- Corretto8JRE/tools/chocolateyinstall.ps1
+++ Corretto8JRE/tools/chocolateyinstall.ps1
@@ -1,15 +1,10 @@
-﻿$programFiles = (${env:ProgramFiles}, ${env:ProgramFiles(x86)} -ne $null)[0]
-$installDir = "$programFiles\Corretto"
-
-$packageArgs = @{
-    PackageName      = $env:ChocolateyPackageName
-    UnzipLocation    = $targetDir = $installDir
-    Url64            = 'https://corretto.aws/downloads/resources/8.322.06.1/amazon-corretto-8.322.06.1-windows-x64-jre.zip'
-    Checksum64       = 'fb75ebb184bf43791af8758415e98acf'
-    ChecksumType64   = 'md5'
+﻿$packageArgs = @{
+  PackageName = $env:ChocolateyPackageName
+  Url64bit = 'https://corretto.aws/downloads/resources/8.332.08.1/amazon-corretto-8.332.08.1-windows-x64-jre.msi'
+  Checksum64 = 'baf2850c7e6ad117308b19f0657b6859'
+  ChecksumType64 = 'md5'
+  fileType      = 'msi'
+  silentArgs    = "INSTALLLEVEL=3 /quiet"
 }
 
-Install-ChocolateyZipPackage @packageArgs
-Install-ChocolateyEnvironmentVariable 'JAVA_HOME' $targetDir\jre8 'Machine'
-# The full path instead of the %JAVA_HOME% is needed so it can be removed with the Chocolatey Uninstall
-Install-ChocolateyPath $targetDir\jre8\bin -PathType 'Machine'
+Install-ChocolateyPackage @packageArgs

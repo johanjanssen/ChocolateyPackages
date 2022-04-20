a817ad51aafa1427a6ab57ec1eebeb2453fb6864
Johan Janssen <johanjanssen2001@gmail.com>
2022-04-20 22:07:06 +0200

    Fixed hash

--- Corretto8JRE/tools/chocolateyinstall.ps1
+++ Corretto8JRE/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$packageArgs = @{
   PackageName = $env:ChocolateyPackageName
   Url64bit = 'https://corretto.aws/downloads/resources/8.332.08.1/amazon-corretto-8.332.08.1-windows-x64-jre.msi'
-  Checksum64 = 'baf2850c7e6ad117308b19f0657b6859'
+  Checksum64 = '4588FF50E3EFA50224047B1383F99DD3'
   ChecksumType64 = 'md5'
   fileType      = 'msi'
   silentArgs    = "INSTALLLEVEL=3 /quiet"

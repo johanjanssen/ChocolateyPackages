080e745b3aec8e6eace09d968ac9a14c77ec445d
Johan Janssen <johanjanssen2001@gmail.com>
2022-05-11 20:19:32 +0200

    Upgrade mvndaemon to 0.8.0 and Microsoft OpenJDK to 11.0.15

--- mvndaemon/tools/chocolateyinstall.ps1
+++ mvndaemon/tools/chocolateyinstall.ps1
@@ -4,10 +4,10 @@ $installDir = "$programFiles\mvndaemon"
 $packageArgs = @{
     PackageName      = $env:ChocolateyPackageName
     UnzipLocation    = $targetDir = $installDir
-    Url64            = 'https://github.com/mvndaemon/mvnd/releases/download/0.7.1/mvnd-0.7.1-windows-amd64.zip'
-    Checksum64       = '749f647df21977e7ab8f1296ae39dedf1067aaaf173eddfb685b73e01d8352ff'
+    Url64            = 'https://dist.apache.org/repos/dist/release/maven/mvnd/0.8.0/maven-mvnd-0.8.0-windows-amd64.zip'
+    Checksum64       = 'eda5d83085b2ddf3c31ba849c0f72a340e9ac5f4b3f002dce060b3fad7733a84'
     ChecksumType64   = 'sha256'
 }
 
 Install-ChocolateyZipPackage @packageArgs
-Install-ChocolateyPath $targetDir\mvnd-0.7.1-windows-amd64\bin -PathType 'Machine'
+Install-ChocolateyPath $targetDir\mvnd-0.8.0-windows-amd64\bin -PathType 'Machine'
--- mvndaemon/tools/chocolateyuninstall.ps1
+++ mvndaemon/tools/chocolateyuninstall.ps1
@@ -1,4 +1,4 @@
 ﻿$programFiles = (${env:ProgramFiles}, ${env:ProgramFiles(x86)} -ne $null)[0]
 $installDir = "$programFiles\mvndaemon"
 
-rm -r "$installDir\mvnd-0.7.1-windows-amd64"
+rm -r "$installDir\mvnd-0.8.0-windows-amd64"

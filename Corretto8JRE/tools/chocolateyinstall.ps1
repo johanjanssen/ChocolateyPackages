eb9217f9709dcfdac488d23c83d0d994f174f894
Johan Janssen <johanjanssen2001@gmail.com>
2022-09-13 20:00:16 +0200

    Updated Corretto 8 JRE to 8.342

--- Corretto8JRE/tools/chocolateyinstall.ps1
+++ Corretto8JRE/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$packageArgs = @{
   PackageName = $env:ChocolateyPackageName
-  Url64bit = 'https://corretto.aws/downloads/resources/8.332.08.1/amazon-corretto-8.332.08.1-windows-x64-jre.msi'
-  Checksum64 = '4588FF50E3EFA50224047B1383F99DD3'
+  Url64bit = 'https://corretto.aws/downloads/resources/8.342.07.3/amazon-corretto-8.342.07.3-windows-x64-jre.msi'
+  Checksum64 = '3f4495c944fedb5d876615f28dce5319'
   ChecksumType64 = 'md5'
   fileType      = 'msi'
   silentArgs    = "INSTALLLEVEL=3 /quiet"

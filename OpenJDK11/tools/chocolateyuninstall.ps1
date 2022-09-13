3871f0ba9593ea28759d68d7b627da26735758f1
Johan Janssen <johanjanssen2001@gmail.com>
2022-09-13 21:09:48 +0200

    Deprecated OpenJDK and changed ojdkbuild deprecated dependency replacements to match the version.

--- OpenJDK11/tools/chocolateyuninstall.ps1
+++
@@ -1,5 +0,0 @@
-﻿$programFiles = (${env:ProgramFiles}, ${env:ProgramFiles(x86)} -ne $null)[0]
-$installDir = "$programFiles\OpenJDK"
-
-Uninstall-ChocolateyEnvironmentVariable 'JAVA_HOME' 'Machine'
-rm -r "$installDir\openjdk-11.0.16_8"

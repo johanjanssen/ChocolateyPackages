70cbb0a3b788d49975dce931ed3d4859ea6d8a1e
johanjanssen <johanjanssen@users.noreply.github.com>
2022-09-13 19:17:55 +0000

    [create-pull-request] automated change

--- Liberica17JDKFull/tools/chocolateyinstall.ps1
+++ Liberica17JDKFull/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$packageArgs = @{
   PackageName = $env:ChocolateyPackageName
-  Url64bit = 'https://github.com/bell-sw/Liberica/releases/download/17.0.4%2B8/bellsoft-jdk17.0.4%2B8-windows-amd64-full.msi'
-  Checksum64 = '0ec3c26b2aaed62e83991b481cd10c52c8c6f2a1'
+  Url64bit = 'https://github.com/bell-sw/Liberica/releases/download/17.0.4.1%2B1/bellsoft-jdk17.0.4.1%2B1-windows-amd64-full.msi'
+  Checksum64 = 'null'
   ChecksumType64 = 'sha1'
   fileType      = 'msi'
   silentArgs    = "INSTALLLEVEL=3 /quiet"

112bab14d0cfb0a1a0565ff6d7b1313751471606
Johan Janssen <johanjanssen2001@gmail.com>
2022-09-20 16:51:23 +0200

    Upgraded Corretto to 19.0.0

--- CorrettoJDK/tools/chocolateyinstall.ps1
+++ CorrettoJDK/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$packageArgs = @{
   PackageName = $env:ChocolateyPackageName
-  Url64bit = 'https://corretto.aws/downloads/resources/18.0.2.9.1/amazon-corretto-18.0.2.9.1-windows-x64.msi'
-  Checksum64 = '99cecaa2b742ead919f1a89e9e411258'
+  Url64bit = 'https://corretto.aws/downloads/resources/19.0.0.36.1/amazon-corretto-19.0.0.36.1-windows-x64.msi'
+  Checksum64 = '4835dbce31d89ddc653e3526309e40c0'
   ChecksumType64 = 'md5'
   fileType      = 'msi'
   silentArgs    = "INSTALLLEVEL=3 /quiet"

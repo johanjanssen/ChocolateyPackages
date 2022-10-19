1ed4c46967aab997d495dedb0786ba9305064bfa
johanjanssen <johanjanssen@users.noreply.github.com>
2022-10-19 18:06:42 +0000

    [create-pull-request] automated change

--- Corretto8JDK/tools/chocolateyinstall.ps1
+++ Corretto8JDK/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$packageArgs = @{
   PackageName = $env:ChocolateyPackageName
-  Url64bit = 'https://corretto.aws/downloads/resources/8.342.07.3/amazon-corretto-8.342.07.3-windows-x64-jdk.msi'
-  Checksum64 = '2883e8bdb3cdcfefd6bfdc659983d520'
+  Url64bit = 'https://corretto.aws/downloads/resources/8.352.08.1/amazon-corretto-8.352.08.1-windows-x64-jdk.msi'
+  Checksum64 = 'a904430d0898c02609216061c13a52aa'
   ChecksumType64 = 'md5'
   fileType      = 'msi'
   silentArgs    = "INSTALLLEVEL=3 /quiet"
--- CorrettoJDK/tools/chocolateyinstall.ps1
+++ CorrettoJDK/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$packageArgs = @{
   PackageName = $env:ChocolateyPackageName
-  Url64bit = 'https://corretto.aws/downloads/resources/19.0.0.36.1/amazon-corretto-19.0.0.36.1-windows-x64.msi'
-  Checksum64 = '4835dbce31d89ddc653e3526309e40c0'
+  Url64bit = 'https://corretto.aws/downloads/resources/19.0.1.10.1/amazon-corretto-19.0.1.10.1-windows-x64.msi'
+  Checksum64 = 'a2fb972f6da27412cad0150426960413'
   ChecksumType64 = 'md5'
   fileType      = 'msi'
   silentArgs    = "INSTALLLEVEL=3 /quiet"

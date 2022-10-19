fc77582b0d3b58ab62aa38226ccf843fa42de870
Johan Janssen <johanjanssen2001@gmail.com>
2022-10-19 20:28:15 +0200

    Upgrade OpenJDK to 19.0.1

--- OpenJDK/tools/chocolateyinstall.ps1
+++ OpenJDK/tools/chocolateyinstall.ps1
@@ -4,12 +4,12 @@ $installDir = "$programFiles\OpenJDK"
 $packageArgs = @{
     PackageName      = $env:ChocolateyPackageName
     UnzipLocation    = $targetDir = $installDir
-    Url64            = 'https://download.java.net/java/GA/jdk19/877d6127e982470ba2a7faa31cc93d04/36/GPL/openjdk-19_windows-x64_bin.zip'
-    Checksum64       = '8fabcee7c4e8d3b53486777ecd27bb906d67d7c1efd1bf22a8290cf659afa487'
+    Url64            = 'https://download.java.net/java/GA/jdk19.0.1/afdd2e245b014143b62ccb916125e3ce/10/GPL/openjdk-19.0.1_windows-x64_bin.zip'
+    Checksum64       = 'adb1a33c07b45c39b926bdeeadf800f701be9c3d04e0deb543069e5f09856185'
     ChecksumType64   = 'sha256'
 }
 
 Install-ChocolateyZipPackage @packageArgs
-Install-ChocolateyEnvironmentVariable 'JAVA_HOME' $targetDir\jdk-19 'Machine'
+Install-ChocolateyEnvironmentVariable 'JAVA_HOME' $targetDir\jdk-19.0.1 'Machine'
 # The full path instead of the %JAVA_HOME% is needed so it can be removed with the Chocolatey Uninstall
-Install-ChocolateyPath $targetDir\jdk-19\bin -PathType 'Machine'
+Install-ChocolateyPath $targetDir\jdk-19.0.1\bin -PathType 'Machine'

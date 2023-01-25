45570a1557595eaac31060a3ad7abb2848497d70
johanjanssen <johanjanssen@users.noreply.github.com>
2023-01-25 19:35:31 +0000

    [create-pull-request] automated change

--- GraalVM/tools/chocolateyinstall.ps1
+++ GraalVM/tools/chocolateyinstall.ps1
@@ -1,12 +1,12 @@
 ﻿$programFiles = (${env:ProgramFiles}, ${env:ProgramFiles(x86)} -ne $null)[0]
 $installDir = "$programFiles\GraalVM"
-$version = "22.3.0"
+$version = "22.3.1"
 
 $packageArgs = @{
     PackageName = $env:ChocolateyPackageName
     UnzipLocation = $targetDir = $installDir
-    Url64bit = 'https://github.com/graalvm/graalvm-ce-builds/releases/download/vm-22.3.0/graalvm-ce-java17-windows-amd64-22.3.0.zip'
-    Checksum64 = '5a6f1d640034166a0fefd9d8924c12b040f120cd019b46b40895ccc8e9a9fc9e'
+    Url64bit = 'https://github.com/graalvm/graalvm-ce-builds/releases/download/vm-22.3.1/graalvm-ce-java17-windows-amd64-22.3.1.zip'
+    Checksum64 = 'd90610038364b7e6629447add63ba500cfc4033410f89ee3eabc333cccfd96c3'
     ChecksumType64 = 'sha256'
 }
 

645ab87af3f08f3f22d1288a029b3deff6900082
Johan Janssen <johanjanssen2001@gmail.com>
2023-05-25 13:28:44 +0200

    Update chocolateyuninstall.ps1

--- mvndaemon/tools/chocolateyuninstall.ps1
+++ mvndaemon/tools/chocolateyuninstall.ps1
@@ -1,4 +1,4 @@
 ﻿$programFiles = (${env:ProgramFiles}, ${env:ProgramFiles(x86)} -ne $null)[0]
 $installDir = "$programFiles\mvndaemon"
 
-rm -r "$installDir\maven-mvnd-0.8.2-windows-amd64"
+rm -r "$installDir\maven-mvnd-0.9.0-windows-amd64"

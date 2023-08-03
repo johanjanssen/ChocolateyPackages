0ee0dd15eda7328703324267f5da34e258bab38d
vairag22 <71322586+vairag22@users.noreply.github.com>
2023-08-03 10:37:34 +0000

    Update microsoft-openjdk11 to v11.0.20

--- Microsoft-OpenJDK11/tools/chocolateyinstall.ps1
+++ Microsoft-OpenJDK11/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$packageArgs = @{
   PackageName = $env:ChocolateyPackageName
-  Url64bit = 'https://aka.ms/download-jdk/microsoft-jdk-11.0.18-windows-x64.msi'
-  Checksum64 = 'fa0ef7750af784f947f29c41659532d174c11210f05e3e36d1c422993ba8709e'
+  Url64bit = 'https://aka.ms/download-jdk/microsoft-jdk-11.0.20-windows-x64.msi'
+  Checksum64 = 'f0a0e4d5255e7844faa0ae508e7410c0d6bc1d9c0b596f62ff1224e81af341f2'
   ChecksumType64 = 'sha256'
   fileType      = 'msi'
   silentArgs    = "INSTALLLEVEL=3 /quiet"

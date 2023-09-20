ccdc03546b4896e5b1cb4f08a182fbdf0b3f2a85
Johan Janssen <johanjanssen2001@gmail.com>
2023-09-20 12:15:31 +0200

    Update chocolateyinstall.ps1

--- SapMachineJRE/tools/chocolateyinstall.ps1
+++ SapMachineJRE/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$packageArgs = @{
   PackageName = $env:ChocolateyPackageName
-  Url64bit = 'https://github.com/SAP/SapMachine/releases/download/sapmachine-21/sapmachine-jre-21_windows-x64_bin.zip'
-  Checksum64 = 'f747b9ee576e5653d64a34385324113fcc7af9f3e12b8316317fd50b062e466b'
+  Url64bit = 'https://github.com/SAP/SapMachine/releases/download/sapmachine-21/sapmachine-jre-21_windows-x64_bin.msi'
+  Checksum64 = '46b507395253a252c518aec8f70c492124bbff7dc69464dc8a84bcd01a5e05c8'
   ChecksumType64 = 'sha256'
   fileType      = 'msi'
   silentArgs    = "INSTALLLEVEL=3 /quiet"

46e1a70f7e9024ead3074a0447d3ec7edeec86fe
johanjanssen <johanjanssen@users.noreply.github.com>
2023-09-21 05:37:02 +0000

    [create-pull-request] automated change

--- LibericaJDK/tools/chocolateyinstall.ps1
+++ LibericaJDK/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$packageArgs = @{
   PackageName = $env:ChocolateyPackageName
-  Url64bit = 'https://github.com/bell-sw/Liberica/releases/download/20.0.2%2B10/bellsoft-jdk20.0.2%2B10-windows-amd64.msi'
-  Checksum64 = 'd8457a4704d0dcf04f69e882104dc05fbaf28be6'
+  Url64bit = 'https://github.com/bell-sw/Liberica/releases/download/21%2B37/bellsoft-jdk21%2B37-windows-amd64.msi'
+  Checksum64 = '9ec87a3410749cfe0f5d151f75fcbea3f05bdec9'
   ChecksumType64 = 'sha1'
   fileType      = 'msi'
   silentArgs    = "INSTALLLEVEL=3 /quiet"
--- LibericaJDKLite/tools/chocolateyinstall.ps1
+++ LibericaJDKLite/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$packageArgs = @{
   PackageName = $env:ChocolateyPackageName
-  Url64bit = 'https://github.com/bell-sw/Liberica/releases/download/20.0.2%2B10/bellsoft-jdk20.0.2%2B10-windows-amd64-lite.msi'
-  Checksum64 = 'a3f3e0004dfc5b2823718236fe3371fcf3ef009e'
+  Url64bit = 'https://github.com/bell-sw/Liberica/releases/download/21%2B37/bellsoft-jdk21%2B37-windows-amd64-lite.msi'
+  Checksum64 = '9d3f29fde17d3141c805dcd45ddcd28b944a2d30'
   ChecksumType64 = 'sha1'
   fileType      = 'msi'
   silentArgs    = "INSTALLLEVEL=3 /quiet"

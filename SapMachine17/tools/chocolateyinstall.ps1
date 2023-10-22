94a3073aafb1c7f3c579245b3dac5eb4ba47daf7
johanjanssen <johanjanssen@users.noreply.github.com>
2023-10-22 18:15:41 +0000

    [create-pull-request] automated change

--- SapMachine17/tools/chocolateyinstall.ps1
+++ SapMachine17/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$packageArgs = @{
   PackageName = $env:ChocolateyPackageName
-  Url64bit = 'https://github.com/SAP/SapMachine/releases/download/sapmachine-17.0.8/sapmachine-jdk-17.0.8_windows-x64_bin.msi'
-  Checksum64 = '0d15003d81f8f49b6fb13c787fe483fa6060811a37a120703e3270df4886e85b'
+  Url64bit = 'https://github.com/SAP/SapMachine/releases/download/sapmachine-17.0.9/sapmachine-jdk-17.0.9_windows-x64_bin.msi'
+  Checksum64 = 'e94736b15ca932e10aeac44017597998c518ebeb11728f22961fc4747c0e26cc'
   ChecksumType64 = 'sha256'
   fileType      = 'msi'
   silentArgs    = "INSTALLLEVEL=3 /quiet"
